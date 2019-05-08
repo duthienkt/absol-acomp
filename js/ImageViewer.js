000fb4c66cab8f74d8a75ccec9d57e6680b8c518
duthienkt <blueskythien2010@live.com>
2019-05-08 17:56:21 +0700

    + init imageview

---
+++ js/ImageViewer.js
@@ -0,0 +1,15 @@
+import Acore from "../ACore";
+
+var _ = Acore._;
+var $ = Acore.$;
+
+function ImageViewer() {
+    var res = _({
+        class: 'absol-image-viewer'
+    });
+
+    return res;
+}
+
+
+export default ImageViewer;
