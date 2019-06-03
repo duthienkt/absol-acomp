eac16b7723c4a17fcfc21c80f1598256a5335ac2
duthienkt <blueskythien2010@live.com>
2019-06-03 14:10:51 +0700

    - auto font

--- dev.js
+++ dev.js
@@ -3,7 +3,7 @@ import IFrameBridge from 'absol/src/Network/IFrameBridge';
 
 
 //for export to window
-window.absol =  window.absol||{};
+window.absol = window.absol || {};
 absol.AComp = AComp;
 
 absol.ShareDom = absol.AComp;
@@ -15,3 +15,23 @@ absol.buildDom = absol.ShareDom._;
 
 window.AComp = absol.AComp;
 window.IFrameBridge = IFrameBridge;
+setTimeout(function () {
+    var mdiLink = absol.$('link', document.head, function (e) {
+        if (e.href && e.href.toLowerCase().indexOf('materialdesignicons') >= 0) return true;
+    });
+    if (!mdiLink) {
+        mdiLink = absol._({
+            tag: 'link',
+            attr: {
+                rel: 'stylesheet',
+                href: '/css/materialdesignicons/materialdesignicons.min.css',
+            },
+            once: {
+                error: function () {
+                    console.warn('/css/materialdesignicons/materialdesignicons.min.css not found');
+                    this.href = 'https://volcanion.cf/materialdesignicons/materialdesignicons.min.css';
+                }
+            }
+        }).addTo(document.head);
+    }
+}, 100);
