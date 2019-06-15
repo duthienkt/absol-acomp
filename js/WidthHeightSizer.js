432c065b3b704309ac37ed9407a20fe97f504e92
duthienkt <blueskythien2010@live.com>
2019-06-15 17:01:22 +0700

    - 4 orient resizer

--- js/WidthHeightSizer.js
+++ js/WidthHeightSizer.js
@@ -8,24 +8,35 @@ var $ = Acore.$;
 
 function WidthHeightResizer() {
     var res = _({
-        extendEvent:'sizechange',
+        extendEvent: 'sizechange',
         class: 'absol-width-height-resizer',
-        child: ['.absol-width-height-resizer-content', '.absol-width-height-resizer-anchor', '.absol-width-height-resizer-anchor-top']
+        child: ['.absol-width-height-resizer-content',
+            '.absol-width-height-resizer-anchor-bot-right',
+            '.absol-width-height-resizer-anchor-bot-left',
+            '.absol-width-height-resizer-anchor-top-right',
+            '.absol-width-height-resizer-anchor-top-left'
+        ]
     });
 
     res.eventHandler = OOP.bindFunctions(res, WidthHeightResizer.eventHandler);
-    res.$anchor = $('.absol-width-height-resizer-anchor', res);
-    res.$anchorTop = $('.absol-width-height-resizer-anchor-top', res);
+
+    res.$anchorBotRight = $('.absol-width-height-resizer-anchor-bot-right', res);
+    res.$anchorTopRight = $('.absol-width-height-resizer-anchor-top-right', res);
+
+    res.$anchorBotLeft = $('.absol-width-height-resizer-anchor-bot-left', res);
+    res.$anchorTopLeft = $('.absol-width-height-resizer-anchor-top-left', res);
+
     res.$content = $('.absol-width-height-resizer-content', res);
 
-    Draggable(res.$anchor).on('drag', res.eventHandler.drag).on('predrag', res.eventHandler.preDrag);
-
-    Draggable(res.$anchorTop).on('drag', res.eventHandler.dragTop).on('predrag', res.eventHandler.preDrag);
+    Draggable(res.$anchorBotRight).on('drag', res.eventHandler.dragBotRight).on('predrag', res.eventHandler.preDrag);
+    Draggable(res.$anchorTopRight).on('drag', res.eventHandler.dragTopRight).on('predrag', res.eventHandler.preDrag);
+    Draggable(res.$anchorBotLeft).on('drag', res.eventHandler.dragBotLeft).on('predrag', res.eventHandler.preDrag);
+    Draggable(res.$anchorTopLeft).on('drag', res.eventHandler.dragTopLeft).on('predrag', res.eventHandler.preDrag);
     return res;
 }
 
-['addChild', 'addChildBefore', 'addChildAfter', 'clearChild'].forEach(function(key){
-    WidthHeightResizer.prototype[key] = function(){
+['addChild', 'addChildBefore', 'addChildAfter', 'clearChild'].forEach(function (key) {
+    WidthHeightResizer.prototype[key] = function () {
         return this.$content[key].apply(this.$content, arguments);
     }
 })
@@ -38,8 +49,8 @@ WidthHeightResizer.eventHandler.preDrag = function (event) {
 
 };
 
-WidthHeightResizer.eventHandler.drag = function (event) {
-    var newEvent = {target: this, data:{}};
+WidthHeightResizer.eventHandler.dragBotRight = function (event) {
+    var newEvent = { target: this, data: {} };
     if (event.moveDX != 0) {
         this.addStyle('width', this._whrWidth + event.moveDX + 'px');
         newEvent.data.changeWidth = true;
@@ -53,10 +64,40 @@ WidthHeightResizer.eventHandler.drag = function (event) {
     this.emit('sizechange', newEvent);
 };
 
-WidthHeightResizer.eventHandler.dragTop = function (event) {
-    var newEvent = {target: this, data:{}};
+WidthHeightResizer.eventHandler.dragTopRight = function (event) {
+    var newEvent = { target: this, data: {} };
     if (event.moveDX != 0) {
         this.addStyle('width', this._whrWidth + event.moveDX + 'px');
+        newEvent.data.changeWidth = true;
+    }
+    if (event.moveDY != 0) {
+        this.addStyle('height', this._whrHeight - event.moveDY + 'px');
+        newEvent.data.changeHeight = true;
+    }
+    newEvent.data.height = this.getComputedStyleValue('height');
+    newEvent.data.width = this.getComputedStyleValue('width');
+    this.emit('sizechange', newEvent);
+};
+
+WidthHeightResizer.eventHandler.dragBotLeft = function (event) {
+    var newEvent = { target: this, data: {} };
+    if (event.moveDX != 0) {
+        this.addStyle('width', this._whrWidth - event.moveDX + 'px');
+        newEvent.data.changeWidth = true;
+    }
+    if (event.moveDY != 0) {
+        this.addStyle('height', this._whrHeight + event.moveDY + 'px');
+        newEvent.data.changeHeight = true;
+    }
+    newEvent.data.height = this.getComputedStyleValue('height');
+    newEvent.data.width = this.getComputedStyleValue('width');
+    this.emit('sizechange', newEvent);
+};
+
+WidthHeightResizer.eventHandler.dragTopLeft = function (event) {
+    var newEvent = { target: this, data: {} };
+    if (event.moveDX != 0) {
+        this.addStyle('width', this._whrWidth - event.moveDX + 'px');
         newEvent.data.changeWidth = true;
     }
     if (event.moveDY != 0) {
