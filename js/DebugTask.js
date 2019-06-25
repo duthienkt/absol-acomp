72622743252fa2e1bcbaa46a4303aa5d298f7703
duthienkt <blueskythien2010@live.com>
2019-06-25 17:17:01 +0700

    - work percent

--- js/DebugTask.js
+++ js/DebugTask.js
@@ -41,14 +41,30 @@ function DebugTask() {
                     tag: 'span',
                     class: ['absol-debug-task-value', 'setinterval'],
                     child: { text: '0' }
-                }]
+                },
+                ]
+            },
+            {
+                child: [{
+                    tag: 'span',
+                    class: 'absol-debug-task-name',
+                    child: { text: 'Work ' }
+                },
+                {
+                    tag: 'span',
+                    class: ['absol-debug-task-value', 'work'],
+                    child: { text: '0%' }
+                },
+                ]
             }
         ]
     });
     res._timeout = 0;
     res._interval = 0;
+    res._work = 0;
     res.$setTimeOutValue = $('.absol-debug-task-value.settimeout', res);
     res.$setIntervalValue = $('.absol-debug-task-value.setinterval', res);
+    res.$setWorkValue = $('.absol-debug-task-value.work', res);
     return res;
 }
 
@@ -74,6 +90,17 @@ DebugTask.property.interval = {
     }
 };
 
+DebugTask.property.work = {
+    set: function (value) {
+        this._work = value;
+        value = (value * 100).toFixed(1);
+        this.$setWorkValue.innerHTML = value + '%';
+    },
+    get: function () {
+        return this._work;
+    }
+};
+
 
 DebugTask.start = function () {
     if (DebugTask.started) return;
@@ -81,20 +108,44 @@ DebugTask.start = function () {
         DebugTask.$view = _('debugtask');
     }
 
+    var times = [];
+    originSetInterval(function(){
+        var now = performance.now();
+        while (times.length > 0 && times[0].end < now - 2000) {
+            times.shift();
+        }
+        if (times.length == 0)  DebugTask.$view.work = 0;
+    },3000);
     global.setTimeout = function () {
         var args = Array.prototype.map.call(arguments, function (x) { return x; });
         var originCallback = arguments[0];
         if (typeof originCallback == 'undefined') return;
-        if (typeof originCallback == 'string'){
+        if (typeof originCallback == 'string') {
             originCallback = new Function(originCallback);
         }
         args[0] = function () {
             var ret;
-            try{
+            try {
+                var now = performance.now();
+                while (times.length > 0 && times[0].end < now - 1000) {
+                    times.shift();
+                }
                 ret = originCallback.apply(null, arguments);
+                var now1 = performance.now();
+                var long = now1 - now;
+                times.push({
+                    long:long,
+                    start:now,
+                    end:now1
+                });
+                var sTime = 0;
+                for (var i = 0; i< times.length; ++i ){
+                    sTime+= times[i].long;
+                }
+                DebugTask.$view.work = sTime/(Math.max(now1 - times[0].start, 1000));
 
             }
-            catch(e){
+            catch (e) {
                 console.error(e);
             }
             if (timeoutDict[timeoutId]) {
@@ -113,7 +164,7 @@ DebugTask.start = function () {
         return timeoutId;
     }
 
-    global.clearTimeout = function(timeoutId){
+    global.clearTimeout = function (timeoutId) {
         var args = Array.prototype.map.call(arguments, function (x) { return x; });
         if (timeoutDict[timeoutId]) {
             pendingTimeout--;
@@ -129,16 +180,31 @@ DebugTask.start = function () {
         var args = Array.prototype.map.call(arguments, function (x) { return x; });
         var originCallback = arguments[0];
         if (typeof originCallback == 'undefined') return;
-        if (typeof originCallback == 'string'){
+        if (typeof originCallback == 'string') {
             originCallback = new Function(originCallback);
         }
         args[0] = function () {
             var ret;
-            try{
+            try {
+                var now = performance.now();
+                while (times.length > 0 && times[0].end < now - 1000) {
+                    times.shift();
+                }
                 ret = originCallback.apply(null, arguments);
-
+                var now1 = performance.now();
+                var long = now1 - now;
+                times.push({
+                    long:long,
+                    start:now,
+                    end:now1
+                });
+                var sTime = 0;
+                for (var i = 0; i< times.length; ++i ){
+                    sTime+= times[i].long;
+                }
+                DebugTask.$view.work = sTime/(Math.max(now1 - times[0].start, 1000));
             }
-            catch(e){
+            catch (e) {
                 console.error(e);
             }
             return ret;
@@ -146,13 +212,13 @@ DebugTask.start = function () {
         var intervalId = originSetInterval.apply(global, args)
         pendingInterval++;
         intervalDict[intervalId] = true;
-    
+
         DebugTask.$view.interval = pendingInterval;
-    
+
         return intervalId;
     }
-    
-    global.clearInterval = function(intervalId){
+
+    global.clearInterval = function (intervalId) {
         var args = Array.prototype.map.call(arguments, function (x) { return x; });
         if (intervalDict[intervalId]) {
             pendingInterval--;
@@ -160,7 +226,7 @@ DebugTask.start = function () {
             DebugTask.$view.interval = pendingInterval;
         }
         return originClearInterval.apply(global, args);
-    
+
     };
 
     Dom.documentReady.then(function () {
