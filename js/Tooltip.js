6b1294c765821428962a98792ea633c2f29c4f00
duthienkt <blueskythien2010@live.com>
2019-08-15 11:04:27 +0700

    + show tooltip

--- js/Tooltip.js
+++ js/Tooltip.js
@@ -1,5 +1,6 @@
 import Acore from "../ACore";
 import Dom from "absol/src/HTML5/Dom";
+import EventEmitter from "absol/src/HTML5/EventEmitter";
 
 var _ = Acore._;
 var $ = Acore.$;
@@ -30,33 +31,18 @@ Acore.install('tooltip', ToolTip);
 
 ToolTip.$root = _('.absol-tooltip-root').addStyle('visibility', 'hidden');
 ToolTip.$holder = _('.absol-tooltip-root-holder').addTo(ToolTip.$root);
-ToolTip.$elt = _('tooltip').addTo(ToolTip.$holder);
+ToolTip.$tooltip = _('tooltip.top').addTo(ToolTip.$holder);
+ToolTip.$element = undefined;
+ToolTip.$content = undefined;
+ToolTip._orientation = 'top';
+ToolTip._session = Math.random() * 10000000000 >> 0;
 
-Dom.documentReady.then(function () {
-    ToolTip.$root.addTo(document.body);
-});
+ToolTip.updatePosition = function () {
+    var element = ToolTip.$element;
+    if (!element) return;
+    var orientation = ToolTip._orientation;
 
-ToolTip.show = function (element, content, orientation) {
-    if (typeof content == 'string') {
-        content = _({
-            tag: 'span',
-            style: {
-                'white-space': 'nowrap'
-            },
-            props: {
-                innerHTML: content
-            }
-        });
-    }
-    ToolTip.$elt.clearChild().addChild(content);
-    ToolTip.$root.removeStyle('visibility');
-
-    ToolTip.$elt.removeClass('top').removeClass('left').removeClass('right').removeClass('bottom');
-    ToolTip.$elt.addClass(orientation);
-
-    var fontSize = ToolTip.$elt.getFontSize();
-    var tBound = ToolTip.$elt.getBoundingClientRect();
-
+    var tBound = ToolTip.$tooltip.getBoundingClientRect();
 
     var rBound = ToolTip.$root.getBoundingClientRect();
     var ebound = element.getBoundingRecursiveRect();
@@ -68,11 +54,12 @@ ToolTip.show = function (element, content, orientation) {
         dx = ebound.left + ebound.width / 2 - rBound.left - tBound.width / 2;
     }
     else if (orientation == 'left') {
-        dy = ebound.top + ebound.height/2  - rBound.top - tBound.height;
-        dx = ebound.left  - rBound.left - tBound.width / 2;
+        dy = ebound.top + ebound.height / 2 - rBound.top - tBound.height / 2;
+        dx = ebound.left - rBound.left - tBound.width;
     }
     else if (orientation == 'right') {
-
+        dy = ebound.top + ebound.height / 2 - rBound.top - tBound.height / 2;
+        dx = ebound.right - rBound.left;
     }
     else if (orientation == 'bottom') {
         dy = ebound.bottom - rBound.top;
@@ -83,28 +70,86 @@ ToolTip.show = function (element, content, orientation) {
         throw new Error("Invalid orientation, orientation:['left', 'right', 'top', 'bottom', 'auto'] ");
     }
 
-    console.log({
-        top: dx + 'px',
-        left: dy + 'px'
-    });
-
-    
     ToolTip.$holder.addStyle({
         top: dy + 'px',
         left: dx + 'px'
     });
-
 };
+
+ToolTip.$tooltip.$arrow.updateSize = ToolTip.updatePosition.bind(ToolTip);
+
+
+ToolTip.show = function (element, content, orientation) {
+
+    if (typeof content == 'string') {
+        content = _({
+            tag: 'span',
+            style: {
+                'white-space': 'nowrap'
+            },
+            props: {
+                innerHTML: content
+            }
+        });
+    }
+
+    //wait for content load
+    $('', content, function (elt) {
+        if (elt.tagName == "IMG" && elt.src) {
+            Dom.waitImageLoaded(elt).then(ToolTip.updatePosition.bind(ToolTip));;
+        }
+        else if (elt.sync) {
+            elt.sync.then(ToolTip.updatePosition.bind(ToolTip));
+        }
+    });
+    var currentSession = Math.random() * 10000000000 >> 0;
+
+    ToolTip.$root.addTo(document.body);
+    Dom.addToResizeSystem(ToolTip.$tooltip.$arrow);
+
+    ToolTip.$element = element;
+    ToolTip._session = currentSession;
+    ToolTip.$content = content;
+    ToolTip._orientation = orientation;
+
+    ToolTip.$tooltip.clearChild().addChild(content);
+    ToolTip.$root.removeStyle('visibility');
+    ToolTip.$tooltip.removeClass('top').removeClass('left').removeClass('right').removeClass('bottom');
+    ToolTip.$tooltip.addClass(orientation);
+    ToolTip.updatePosition();
+    return currentSession;
+};
+
+ToolTip.closeTooltip = function (session) {
+    if (session === true || session === this._session) {
+        ToolTip.$root.addStyle('visibility', 'hidden');
+        ToolTip.$tooltip.clearChild();
+        ToolTip.$holder.addStyle({
+            top: false,
+            left: false
+        });
+    }
+}
 
 
 ToolTip.showWhenClick = function (element, content, orientation) {
+    var mSession = undefined;
+    element.addEventListener('click', function () {
+        if (mSession !== undefined) return;
+        mSession = ToolTip.show(element, content, orientation);
+        var finish = function (event) {
+            if (!EventEmitter.hitElement(content, event)) {
+                $(document.body).off('click', finish);
+                ToolTip.closeTooltip(mSession);
+                mSession = undefined;
+            }
+        };
 
+        setTimeout(function () {
+            $(document.body).on('click', finish);
+        }, 100)
+    });
 };
-
-ToolTip.showWhenOver = function (element, content, timeout, orientation) {
-
-};
-
 
 
 export default ToolTip;
--- webpack.config.js
+++ webpack.config.js
@@ -49,8 +49,5 @@ module.exports = {
     },
     performance: {
         hints: false
-    },
-    plugins: [
-        new MinifyPlugin()
-    ]
+    }
 };
