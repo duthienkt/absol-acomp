688692d0bed1bd1633410c57b3bc13013c6216dc
duthienkt <blueskythien2010@live.com>
2019-09-26 18:29:42 +0700

    - utils

--- js/utils.js
+++ js/utils.js
@@ -29,3 +29,61 @@ export function contenteditableTextOnly(element, processText) {
         }
     });
 }
+
+
+export function measureText(text, font) {
+    // re-use canvas object for better performance
+    var canvas = measureText.canvas || (measureText.canvas = document.createElement("canvas"));
+    var context = canvas.getContext("2d");
+    if (font)
+        context.font = font;
+    var metrics = context.measureText(text);
+    return metrics;
+}
+
+
+export function preventNotNumberInput(elt) {
+    function getCaretPosition(oField) {
+        var iCaretPos = 0;
+        if (document.selection) {
+            oField.focus();
+            var oSel = document.selection.createRange();
+            oSel.moveStart('character', -oField.value.length);
+            iCaretPos = oSel.text.length;
+        }
+        else if (oField.selectionStart || oField.selectionStart == '0')
+            iCaretPos = oField.selectionDirection == 'backward' ? oField.selectionStart : oField.selectionEnd;
+        return iCaretPos;
+    }
+
+    elt.addEventListener('keyup', function () {
+        var lastValue = self.attributes.value;
+        var cValue = parseFloat(this.value);
+        if (this.value != lastValue) {
+            self.attributes.value = cValue;
+            self.emit('change', cValue, self);
+        }
+    }).addEventListener("paste", function (e) {
+        e.preventDefault();
+        var text = "";
+        if (e.clipboardData && e.clipboardData.getData) {
+            text = e.clipboardData.getData("text/plain");
+
+        } else if (window.clipboardData && window.clipboardData.getData) {
+            text = window.clipboardData.getData("Text");
+        }
+        var matched = text.match(/[+-]?([0-9]*[.])?[0-9]+/);
+        if (matched) {
+            this.value = matched[0];
+        }
+    }).addEventListener('keydown', function (event) {
+        var key = event.key;
+        if (key && key.length == 1) {
+            if (key.match(/[0-9.\-\+]/)) {
+                if (key == '.' && this.value.indexOf('.') >= 0) event.preventDefault();
+                if ((key == '+' || key == '-') && (this.value.indexOf('+') >= 0 || this.value.indexOf('-') >= 0 || getCaretPosition(this) > 0)) event.preventDefault();
+            }
+            else event.preventDefault();
+        }
+    });
+}
