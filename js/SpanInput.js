1e9a9d8e9dc564e2d92ffb02f73b807c7b0af651
duthienkt <blueskythien2010@live.com>
2019-11-13 14:19:31 +0700

    - more event

--- js/SpanInput.js
+++ js/SpanInput.js
@@ -1,5 +1,5 @@
 import Acore from "../ACore";
-import { contenteditableTextOnly, insertTextAtCursor } from "./utils";
+import { insertTextAtCursor } from "./utils";
 import { getTextIn } from "absol/src/HTML5/Text";
 
 var _ = Acore._;
@@ -12,14 +12,24 @@ function SpanInput() {
     this._lastValue = null;
     this.on('paste', this.eventHandler.paste)
         .on('keyup', this.eventHandler.keyup)
-        .on('keydown', this.eventHandler.keydown);
+        .on('keydown', this.eventHandler.keydown)
+        .on('blur', this.eventHandler.blur);
 }
 
+
+SpanInput.prototype.notifyValueChange = function (event) {
+    var cValue = this.value;
+    if (this._lastValue != cValue && (this._inputType != 1 || !isNaN(this._lastValue) || !isNaN(cValue))) {
+        this._lastValue = cValue;
+        this.emit('change', { type: 'change', value: cValue, originEvent: event }, this);
+    }
+}
 
 SpanInput.eventHandler = {};
 
 SpanInput.eventHandler.keyup = function (event) {
     if (this._inputType == 0) {
+        this.notifyValueChange(event);
     }
     else if (this._inputType == 1) {
         this.eventHandler.numberKeyup(event)
@@ -36,22 +46,43 @@ SpanInput.eventHandler.keydown = function (event) {
 };
 
 
+SpanInput.eventHandler.numberBlur = function () {
+    this.notifyValueChange(event);
+};
+
+
+SpanInput.eventHandler.textBlur = function (event) {
+    this.notifyValueChange(event);
+};
+
+SpanInput.eventHandler.blur = function (event) {
+    if (this._inputType == 0) {
+        this.eventHandler.textBlur(event);
+    }
+    else if (this._inputType == 1) {
+        this.eventHandler.numberKeydown(event)
+    }
+}
 
 
 SpanInput.eventHandler.numberKeyup = function (event) {
-    var cValue = parseFloat(this.value);
-    if (this._lastValue != cValue) {
-        this._lastValue = cValue;
-        this.emit('change', { type: 'change', value: cValue }, this);
-    }
+    this.notifyValueChange();
 };
 
 SpanInput.eventHandler.numberKeydown = function (event) {
     var key = event.key;
+    if (key == 'Enter') {
+        event.preventDefault();
+        this.blur();
+        this.notifyValueChange();
+        return;
+    }
+
+
     if (key && key.length == 1 && !event.ctrlKey && !event.altKey) {
         if (key.match(/[0-9.\-\+]/)) {
-            if (key == '.' && this.value.indexOf('.') >= 0) event.preventDefault();
-            if ((key == '+' || key == '-') && (this.value.indexOf('+') >= 0 || this.value.indexOf('-') >= 0 || getCaretPosition(this) > 0)) event.preventDefault();
+            if (key == '.' && this.text.indexOf('.') >= 0) event.preventDefault();
+            if ((key == '+' || key == '-') && (this.text.indexOf('+') >= 0 || this.text.indexOf('-') >= 0 || getCaretPosition(this) > 0)) event.preventDefault();
         }
         else event.preventDefault();
     }
@@ -64,8 +95,8 @@ SpanInput.eventHandler.paste = function (event) {
         var text = event.clipboardData.getData("text/plain");
         if (this._inputType == 1) text = text.replace(/[^0-9.\-+]/g, '');
         document.execCommand("insertHTML", false, text);
-        if (this._inputType == 1){
-            if (isNaN(this.value)){
+        if (this._inputType == 1) {
+            if (isNaN(this.value)) {
                 this.value = NaN;
             }
         }
@@ -73,8 +104,8 @@ SpanInput.eventHandler.paste = function (event) {
         var text = window.clipboardData.getData("Text");
         if (this._inputType == 1) text = text.replace(/[^0-9.\-+]/g, '');
         insertTextAtCursor(text);
-        if (this._inputType == 1){
-            if (isNaN(this.value)){
+        if (this._inputType == 1) {
+            if (isNaN(this.value)) {
                 this.value = NaN;
             }
         }
@@ -85,7 +116,7 @@ SpanInput.eventHandler.paste = function (event) {
 SpanInput.property = {};
 SpanInput.property.value = {
     set: function (value) {
-        if (this._inputType == 1 && isNaN(value)) value = null;
+        if (this._inputType == 1 && isNaN(value)) value = '';
         this.clearChild();
         if (value !== null) this.addChild(_({ text: value + '' }));
     },
@@ -104,11 +135,11 @@ SpanInput.property.type = {
         var index = ['text', 'number'].indexOf(value);
         if (index < 0) index = 0;
         this._inputType = index;
-        if (index == 1){
+        if (index == 1) {
             this.addClass('absol-span-input-empty-minus');
 
         }
-        else{
+        else {
             this.removeClass('absol-span-input-empty-minus');
         }
     },
@@ -119,8 +150,8 @@ SpanInput.property.type = {
 
 
 SpanInput.property.text = {
-    get:function(){
-        return  getTextIn(this);
+    get: function () {
+        return getTextIn(this);
     }
 }
 
