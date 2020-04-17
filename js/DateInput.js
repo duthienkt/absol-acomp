e092d5ee1a8eca66f1ff764f19399e176ed5faaa
duthienkt <blueskythien2010@live.com>
2020-04-17 19:49:26 +0700

    - support format

--- js/DateInput.js
+++ js/DateInput.js
@@ -1,6 +1,7 @@
 import ACore from "../ACore";
 import { daysInMonth, beginOfDay, compareDate, formartDateString } from "absol/src/Time/datetime";
 import ChromeCalendar from "./ChromeCalendar";
+import OOP from "absol/src/HTML5/OOP";
 
 var _ = ACore._;
 var $ = ACore.$;
@@ -10,6 +11,7 @@ var $ = ACore.$;
 function DateInput() {
     this._lastValue = null;
     this._value = null;
+    this._format = 'dd/mm/yyyy';
     this.$input = $('input', this)
         .on('mouseup', this.eventHandler.mouseup)
         .on('keydown', this.eventHandler.keydown)
@@ -26,8 +28,55 @@ function DateInput() {
         selectedDates: [new Date()],
     }, 'auto', this.eventHandler.calendarSelect);
     this._calendarHolder.element = this;
-
+    this._formater = DateInput.formaters[this._format];
+    OOP.drillProperty(this, this._calendarHolder.calendarProps, ['minLimitDate', 'maxLimitDate']);
+    this.value = null;
 }
+
+DateInput.formaters = {
+    'dd/mm/yyyy': {
+        separator: '/',
+        nan: ['dd', 'mm', 'yyyy'],
+        dayIdx: 0,
+        monthIdx: 1,
+        yearIdx: 2
+    },
+    'dd-mm-yyyy': {
+        separator: '-',
+        nan: ['dd', 'mm', 'yyyy'],
+        dayIdx: 0,
+        monthIdx: 1,
+        yearIdx: 2
+    },
+    'dd.mm.yyyy': {
+        separator: '.',
+        nan: ['dd', 'mm', 'yyyy'],
+        dayIdx: 0,
+        monthIdx: 1,
+        yearIdx: 2
+    },
+    'yyyy/mm/dd': {
+        separator: '/',
+        nan: ['yyyy', 'mm', 'dd'],
+        dayIdx: 2,
+        monthIdx: 1,
+        yearIdx: 0
+    },
+    'yyyy-mm-dd': {
+        separator: '-',
+        nan: ['yyyy', 'mm', 'dd'],
+        dayIdx: 2,
+        monthIdx: 1,
+        yearIdx: 0
+    }, 
+    'mm/dd/yyyy': {
+        separator: '/',
+        nan: ['mm', 'dd', 'yyyy'],
+        dayIdx: 1,
+        monthIdx: 0,
+        yearIdx: 2
+    },
+};
 
 DateInput.render = function () {
     //only support dd/mm/yyyy
@@ -51,7 +100,7 @@ DateInput.render = function () {
 DateInput.prototype._autoSelect = function () {
     var slEnd = this.$input.selectionEnd;
     var slStart = this.$input.selectionStart;
-    var texts = this.$input.value.split('/');
+    var texts = this.$input.value.split(this._formater.separator);
     var lTexts = texts.reduce(function (ac, cr) {
         ac.push(ac[ac.length - 1] + cr.length + 1);
         return ac;
@@ -82,6 +131,25 @@ DateInput.prototype.notifyChange = function () {
     this.emit('change', { type: 'change', target: this, value: this._value }, this);
 }
 
+DateInput.prototype._countSeparator = function (text) {
+    return text.replace(new RegExp('[^\\' + this._formater.separator + ']', 'g'), '').length
+};
+
+DateInput.prototype._cleanCharacter = function (text) {
+    return text.replace(new RegExp('[^0-9\\' + this._formater.separator + ']', 'g'), '');
+};
+
+DateInput.prototype._splitBySeparator = function (text) {
+    return text.split(this._formater.separator);
+};
+
+DateInput.prototype._onlySeparator = function (text) {
+    return text.replace(new RegExp('[^\\' + this._formater.separator + ']', 'g'), '')
+};
+
+DateInput.prototype._isAcceptKey = function (key) {
+    return !!key.match(new RegExp('[0-9\\' + this._formater.separator + ']', 'i'));
+};
 
 /**
  * @type {DateInput}
@@ -91,28 +159,28 @@ DateInput.eventHandler = {};
 DateInput.eventHandler.paste = function (event) {
     var paste = (event.clipboardData || window.clipboardData).getData('text');
     event.preventDefault();
-    paste = paste.replace(/[^0-9\/]/g, '');
+    paste = this._cleanCharacter(paste);
     var slEnd = this.$input.selectionEnd;
     var slStart = this.$input.selectionStart;
     var sStart = Math.min(slStart, slEnd);
     var sEnd = Math.max(slEnd, slStart);
     var value = this.$input.value;
-    var slashPasteCount = paste.replace(/[^\/]/g, '').length;
-    var slashSelectedCount = value.substr(sStart, sEnd - sStart).replace(/[^\/]/g, '').length;
+    var slashPasteCount = this._countSeparator(paste)
+    var slashSelectedCount = this._countSeparator(value.substr(sStart, sEnd - sStart));
 
     if (slashPasteCount < 2) {
         if (slashPasteCount > slashSelectedCount) {
-            paste = paste.split(/[\/]/).slice(0, slashSelectedCount + 1).join('/');
+            paste = this._splitBySeparator(paste).slice(0, slashSelectedCount + 1).join(this._formater.separator);
         }
         else if (slashPasteCount < slashSelectedCount) {
-            paste += '/'.repeat(slashSelectedCount - slashPasteCount);
+            paste += this._formater.separator.repeat(slashSelectedCount - slashPasteCount);
         }
         slStart = (value.substr(0, sStart) + paste).length;
         slEnd = slStart;
         value = value.substr(0, sStart) + paste + value.substr(sEnd);
     }
     else {
-        value = paste.split(/[\/]/).slice(0, 3).join('/');
+        value = this._splitBySeparator(paste).slice(0, 3).join(this._formater.separator);
         slStart = value.length;
         slEnd = value.length;
     }
@@ -128,7 +196,7 @@ DateInput.eventHandler.cut = function (event) {
     var sStart = Math.min(slStart, slEnd);
     var sEnd = Math.max(slEnd, slStart);
     var value = this.$input.value;
-    this.$input.value = value.substr(0, sStart) + value.substr(sStart, sEnd - sStart).replace(/[^\/]/g, '') + value.substr(sEnd);
+    this.$input.value = value.substr(0, sStart) + this._onlySeparator(value.substr(sStart, sEnd - sStart)) + value.substr(sEnd);
     this.$input.selectionStart = slStart;
     this.$input.selectionEnd = slStart;
 };
@@ -142,14 +210,15 @@ DateInput.eventHandler.focus = function () {
 };
 
 DateInput.eventHandler.blur = function () {
+    var thisIp = this;
     this.$input.off('blur', this.eventHandler.blur);
     var value = this.$input.value;
-    var slashValueCount = value.replace(/[^\/]/g, '').length;
-    for (var i = slashValueCount; i < 2; ++i) value += '/';
-    var texts = value.split('/').slice(0, 3);
-    var day = parseInt(texts[0]);
-    var month = parseInt(texts[1]);
-    var year = parseInt(texts[2]);
+    var slashValueCount = this._countSeparator(value).length;
+    for (var i = slashValueCount; i < 2; ++i) value += this._formater.separator;
+    var texts = value.split(this._formater.separator).slice(0, 3);
+    var day = parseInt(texts[this._formater.dayIdx]);
+    var month = parseInt(texts[this._formater.monthIdx]);
+    var year = parseInt(texts[this._formater.yearIdx]);
     if (!isNaN(year)) year = Math.min(2090, Math.max(year, 1890));
     if (!isNaN(month)) month = Math.max(1, Math.min(12, month));
     if (!isNaN(day)) {
@@ -160,7 +229,6 @@ DateInput.eventHandler.blur = function () {
         }
     }
     if (!isNaN(year) && !isNaN(month) && !isNaN(day)) {
-
         var dateValue = new Date(year, month - 1, day, 0, 0, 0, 0);
         if (this._lastValue == null || compareDate(dateValue, this._lastValue) != 0) {
             this.value = dateValue;
@@ -168,10 +236,17 @@ DateInput.eventHandler.blur = function () {
         }
     }
     else {
-        this.$input.value = [day, month, year].map(function (e, i) {
-            if (isNaN(e)) return '__' + (i == 2 ? '__' : '');
-            return (e < 10 ? '0' : '') + e;
-        }).join('/');
+        var values = this._formater.nan.slice();
+        values[this._formater.dayIdx] = day;
+        values[this._formater.monthIdx] = month;
+        values[this._formater.yearIdx] = year;
+
+        this.$input.value = values.map(function (e, i) {
+            if (isNaN(e)) return thisIp._formater.nan[i];
+            e = e + '';
+            while (e.length < thisIp._formater.nan[i].length) e = '0' + e;
+            return e;
+        }).join(this._formater.separator);
         if (this._lastValue != null) {
             this._value = null;
             this.notifyChange();
@@ -192,10 +267,10 @@ DateInput.eventHandler.keydown = function (event) {
     var sStart = Math.min(slStart, slEnd);
     var sEnd = Math.max(slEnd, slStart);
     var selectedValue = value.substr(sStart, sEnd - sStart);
-    var slashValueCount = value.replace(/[^\/]/g, '').length;
-    var slashSelectedCount = value.substr(sStart, sEnd - sStart).replace(/[^\/]/g, '').length;
+    var slashValueCount = this._countSeparator(value);
+    var slashSelectedCount = this._countSeparator(selectedValue);
 
-    var texts = value.split('/');
+    var texts = this._splitBySeparator(value);
     var lTexts = texts.reduce(function (ac, cr) {
         ac.push(ac[ac.length - 1] + cr.length + 1);
         return ac;
@@ -220,7 +295,7 @@ DateInput.eventHandler.keydown = function (event) {
     else if (event.key == 'Backspace') {
         if (slStart == slEnd) {
             if (slStart > 0) {
-                if (value.charAt(slStart - 1) == '/') {
+                if (value.charAt(slStart - 1) == this._formater.separator) {
                     event.preventDefault();
                     this.$input.value = value;
                     this.$input.selectionStart = slStart - 1;
@@ -230,8 +305,7 @@ DateInput.eventHandler.keydown = function (event) {
         }
         else if (i0 != i1) {
             event.preventDefault();
-
-            this.$input.value = value.substr(0, sStart) + selectedValue.replace(/[^\/]/g, '') + value.substr(sEnd);
+            this.$input.value = value.substr(0, sStart) + this._onlySeparator(selectedValue) + value.substr(sEnd);
             this.$input.selectionStart = slStart;
             this.$input.selectionEnd = slStart;
         }
@@ -239,7 +313,7 @@ DateInput.eventHandler.keydown = function (event) {
     else if (event.key == 'Delete') {
         if (slStart == slEnd) {
             if (slStart < value.length) {
-                if (value.charAt(slStart) == '/') {
+                if (value.charAt(slStart) == this._formater.separator) {
                     event.preventDefault();
                     this.$input.value = value;
                     this.$input.selectionStart = slStart + 1;
@@ -249,23 +323,21 @@ DateInput.eventHandler.keydown = function (event) {
         }
         else if (i0 != i1) {
             event.preventDefault();
-            var sStart = Math.min(slStart, slEnd);
-            var sEnd = Math.max(slEnd, slStart);
-            this.$input.value = value.substr(0, sStart) + value.substr(sStart, sEnd - sStart).replace(/[^\/]/g, '') + value.substr(sEnd);
+            this.$input.value = value.substr(0, sStart) + this._onlySeparator(selectedValue) + value.substr(sEnd);
             this.$input.selectionStart = slStart;
             this.$input.selectionEnd = slStart;
         }
     }
     else if (!event.ctrlKey && !event.altKey && event.key && event.key.length == 1) {
-        if (event.key.match(/[0-9\/]/)) {
-            if (event.key == '/') {
-                if (slashSelectedCount == 0 && slashValueCount >= 2 && value.charAt(slStart) != '/') {
+        if (this._isAcceptKey(event.key)) {
+            if (event.key == this._formater.separator) {
+                if (slashSelectedCount == 0 && slashValueCount >= 2 && value.charAt(slEnd) != this._formater.separator) {
                     event.preventDefault();
                 }
-                else if (value.charAt(slStart) == '/') {
+                else if (value.charAt(slEnd) == this._formater.separator) {
                     event.preventDefault();
-                    this.$input.selectionStart = slStart + 1;
-                    this.$input.selectionEnd = slStart + 1;
+                    this.$input.selectionStart = lTexts[i1];
+                    this.$input.selectionEnd = lTexts[i1 + 1] - 1;
                 }
             }
         }
@@ -297,16 +369,16 @@ DateInput.property = {};
 DateInput.property.value = {
     set: function (value) {
         if (value === false || value === null || value === undefined) {
-            this.$input = '__/__/____';
+            this.$input.value = this._format;
             this._value = null;
         }
         else if ((typeof value == 'string') || (typeof value == 'number')) {
             this._value = beginOfDay(new Date(value));
-            this.$input.value = formartDateString(this._value, 'dd/mm/yyyy');
+            this.$input.value = formartDateString(this._value, this._format);
         }
         else if (value.getTime) {
             this._value = beginOfDay(value);
-            this.$input.value = formartDateString(this._value, 'dd/mm/yyyy');
+            this.$input.value = formartDateString(this._value, this._format);
         }
         this._lastValue = this._value;
         this._calendarHolder.calendarProps.selectedDates = [this._value || new Date()];
@@ -315,7 +387,36 @@ DateInput.property.value = {
         return this._value;
     }
 };
-//textContent
+
+DateInput.property.format = {
+    set: function (value) {
+        value = value || 'dd/mm/yyyy';
+        if (value == this._format) return;
+        if (DateInput.formaters[value]) {
+            this._formater = DateInput.formaters[value];
+            this._format = value;
+            this.value = this.value;
+        }
+        else {
+            throw new Error("Not support format " + value);
+        }
+    },
+    get: function () {
+        return this._format;
+    }
+};
+
+DateInput.property.disabled = {
+    set: function(value){
+        value = !!value;
+        this.$input.disabled = value;
+        if (value) this.addClass('as-disabled');
+        else this.removeClass('as-disabled'); 
+    },
+    get: function(){
+        return this.$input.disabled;
+    }
+}
 
 ACore.install('dateinput', DateInput);
 
