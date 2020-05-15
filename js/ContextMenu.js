e8b055f5befa5bdacbf0757cad5f3db266a34511
duthienkt <blueskythien2010@live.com>
2020-05-15 08:20:34 +0700

    - context menu more feature

--- js/ContextMenu.js
+++ js/ContextMenu.js
@@ -1,7 +1,7 @@
 import ACore from "../ACore";
 import EventEmitter from "absol/src/HTML5/EventEmitter";
 import Dom from "absol/src/HTML5/Dom";
-
+import Vec2 from 'absol/src/Math/Vec2';
 
 var _ = ACore._;
 var $ = ACore.$;
@@ -11,6 +11,22 @@ export function ContextCaptor() {
     this.$textarea = $('textarea', this)
         .on('contextmenu', this.eventHandler.contextmenu, true);
     this._ss = 0;
+    this._isTouch = false;
+    /**
+    this._target = null;
+     * @type {Vec2}
+     */
+    this._posStart = null;
+    /**
+     * @type {Vec2}
+     */
+    this._posCurrent = null;
+    this._touchId = -100;
+    this._longPressTimeout = -1;
+    this._removeTimeout = -1;
+
+    this.$target = null;
+
     this.mousedownEvent = null;
     this.sync = Promise.resolve();
 };
@@ -18,11 +34,13 @@ export function ContextCaptor() {
 ContextCaptor.prototype.attachTo = function (elt) {
     if (this.attachedElt) {
         this.attachedElt.removeEventListener('mousedown', this.eventHandler.mousedown);
+        this.attachedElt.removeEventListener('touchstart', this.eventHandler.mousedown);
         this.attachedElt = null;
     }
     this.attachedElt = elt;
     if (this.attachedElt) {
         this.attachedElt.addEventListener('mousedown', this.eventHandler.mousedown);
+        this.attachedElt.addEventListener('touchstart', this.eventHandler.mousedown);
     }
     return this;
 };
@@ -41,8 +59,11 @@ ContextCaptor.render = function () {
 ContextCaptor.prototype.showContextMenu = function (x, y, props, onSelectItems, onCancel) {
     var self = this;
     var anchor = _('.absol-context-menu-anchor').addTo(document.body);
-    var finish = function () {
-        (document.body).off('click', finish);
+    var finish = function (event) {
+        (document.body).off('click', finish)
+            .off('touchcancel', finish)
+            .off('touchend', finish);
+
         self.off('requestcontextmenu', finish);
         setTimeout(function () {
             anchor.selfRemove();
@@ -71,34 +92,90 @@ ContextCaptor.prototype.showContextMenu = function (x, y, props, onSelectItems, 
         });
         anchor.addClass('absol-active');
     }, 30);
-
-    (document.body).on('click', finish);
-    this.on('requestcontextmenu', finish);
+    setTimeout(function () {
+        (document.body).on('click', finish);
+        self.on('requestcontextmenu', finish);
+    }, 10)
 };
 
+ContextCaptor.prototype._checkNeedHandle = function (target) {
+    var current = target;
+    var needHandle = false;
+    while (current && !needHandle && !current.classList.contains('as-system-context-menu')) {
+        if (current.isSupportedEvent && current.isSupportedEvent('contextmenu'))
+            needHandle = true;
+        current = current.parentElement;
+    }
+    return needHandle;
+};
 
 /**
  * @type {ContextCaptor}
  */
 ContextCaptor.eventHandler = {};
+
 ContextCaptor.eventHandler.mousedown = function (event) {
-    if (EventEmitter.isMouseRight(event)) {
-        var current = event.target;
-        var needHandle = false;
-        while (current && !needHandle) {
-            if (current.isSupportedEvent && current.isSupportedEvent('contextmenu'))
-                needHandle = true;
-            current = current.parentElement;
-        }
-        if (needHandle) {
+    if (this._touchId != -100) return;
+
+    var target;
+    var isTouch;
+    var touchId;
+    var posCurrent;
+    var pointer;
+    if (event.type == 'touchstart') {
+        isTouch = true;
+        pointer = event.changedTouches[0];
+        touchId = pointer.identifier;
+    }
+    else {
+        isTouch = false;
+        touchId = -1;
+        pointer = event;
+    }
+    target = pointer.target;
+
+    posCurrent = new Vec2(pointer.clientX, pointer.clientY);
+
+    if (isTouch) {
+        var thisCT = this;
+        this._longPressTimeout = setTimeout(function () {
+            if (!thisCT._checkNeedHandle(target)) return;
+            if (thisCT._removeTimeout > 0) {
+                clearTimeout(thisCT._removeTimeout);
+                thisCT._removeTimeout = -1;
+            }
+
+
+            thisCT._ss++;
+            thisCT.moveTo(thisCT._posCurrent);
+            thisCT.active(true);
+            thisCT._longPressTimeout = -1;
+        }, 500);
+        this.$target = target;
+        this._isTouch = isTouch;
+        this._touchId = touchId;
+        this._posCurrent = posCurrent;
+        this._posStart = posCurrent;
+        $(document.body).on('touchmove', thisCT.eventHandler.mousemove)
+            .on('touchend', thisCT.eventHandler.mousefinish)
+            .on('touchcancel', thisCT.eventHandler.mousefinish);
+
+    }
+    else {
+        if (EventEmitter.isMouseRight(event) && this._checkNeedHandle(target)) {
+            if (this._removeTimeout > 0) {
+                clearTimeout(this._removeTimeout);
+                this._removeTimeout = -1;
+            }
+            this.$target = target;
+            this._isTouch = isTouch;
+            this._posCurrent = posCurrent;
+            this._posStart = posCurrent;
+            this._touchId = touchId;
             this._ss++;
-            this.$target = event.target;
-            this.mousedownEvent = event;
-            this.addStyle({
-                left: event.clientX - 10 + 'px',
-                top: event.clientY - 10 + 'px'
-            });
-            this.addClass('absol-active');
+            this.moveTo(this._posCurrent);
+            this.active(true);
+
             $(document.body).on('mousemove', this.eventHandler.mousemove)
                 .on('mouseup', this.eventHandler.mousefinish)
                 .on('mouseleave', this.eventHandler.mousefinish);
@@ -106,25 +183,88 @@ ContextCaptor.eventHandler.mousedown = function (event) {
     }
 };
 
-ContextCaptor.eventHandler.mousemove = function () {
+/**
+ * @param {Vec2} pos
+ */
+ContextCaptor.prototype.moveTo = function (pos) {
     this.addStyle({
-        left: event.clientX - 10 + 'px',
-        top: event.clientY - 10 + 'px'
+        left: pos.x - 20 + 'px',
+        top: pos.y - 20 + 'px'
     });
+};
+
+ContextCaptor.prototype.active = function (flag) {
+    if (flag)
+        this.addClass('absol-active');
+    else
+        this.removeClass('absol-active');
+};
+
+ContextCaptor.eventHandler.mousemove = function () {
+    var isTouch = this._isTouch;
+    var isTouch;
+    var touchId;
+    var pointer;
+    var posCurrent;
+    if (isTouch) {
+        isTouch = true;
+        pointer = event.changedTouches[0];
+        touchId = pointer.identifier;
+    }
+    else {
+        isTouch = false;
+        touchId = -1;
+        pointer = event;
+    }
+
+    if (touchId != this._touchId) return;
+    posCurrent = new Vec2(pointer.clientX, pointer.clientY);
+    this._posCurrent = posCurrent;
+    if (isTouch) {
+        if (this._posStart.sub(posCurrent).abs() > 10) this.eventHandler.mousefinish(event);
+    }
+    this.moveTo(posCurrent);
 };
 
 
 ContextCaptor.eventHandler.mousefinish = function () {
-    $(document.body).off('mousemove', this.eventHandler.mousemove)
-        .off('mouseup', this.eventHandler.mousefinish)
-        .off('mouseleave', this.eventHandler.mousefinish);
-    var ss = this._ss;
-    var self = this;
-    setTimeout(function () {
-        if (self._ss == ss)
-            self.removeClass('absol-active');
-    }, 30);
-    //todo
+
+    var isTouch = this._isTouch;
+    var touchId;
+    var pointer;
+
+    if (isTouch) {
+        pointer = event.changedTouches[0];
+        touchId = pointer.identifier;
+    }
+    else {
+        isTouch = false;
+        touchId = -1;
+        pointer = event;
+
+    }
+    if (touchId != this._touchId) return;
+    if (isTouch) {
+        $(document.body).off('touchmove', this.eventHandler.mousemove)
+            .off('touchend', this.eventHandler.mousefinish)
+            .off('touchcancel', this.eventHandler.mousefinish);
+        if (this._longPressTimeout > 0) {
+            clearTimeout(this._longPressTimeout);
+            this._longPressTimeout = -1;
+        }
+    }
+    else {
+        $(document.body).off('mousemove', this.eventHandler.mousemove)
+            .off('mouseup', this.eventHandler.mousefinish)
+            .off('mouseleave', this.eventHandler.mousefinish);
+    }
+
+    this._touchId = -100;
+    var thisCT = this;
+    this._removeTimeout = setTimeout(function () {
+        thisCT.active(false);
+        thisCT._removeTimeout = -1;
+    }, 1);
 };
 
 ContextCaptor.eventHandler.contextmenu = function (event) {
@@ -166,7 +306,7 @@ ContextCaptor.auto = function () {
     if (ContextCaptor.$elt) return;
     ContextCaptor.$elt = _('contextcaptor');
     Dom.documentReady.then(function () {
-        ContextCaptor.$elt.addTo(document.body); 
+        ContextCaptor.$elt.addTo(document.body);
         ContextCaptor.$elt.attachTo(document.body);
     });
 };
