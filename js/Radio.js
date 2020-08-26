d6a6e304093d344ef0ddb4c9b887dfd305fff7d4
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-08-26 17:06:37 +0700

    - radio with optimize svg

--- js/Radio.js
+++ js/Radio.js
@@ -2,56 +2,57 @@ import '../css/radio.css';
 import ACore from "../ACore";
 import OOP from "absol/src/HTML5/OOP";
 import RadioButton from "./RadioButton";
+import Svg from "absol/src/HTML5/Svg";
+import AElement from "absol/src/HTML5/AElement";
 
 
 var _ = ACore._;
 var $ = ACore.$;
+var $$ = ACore.$$;
+var _svg = Svg.ShareInstance._;
 
+/***
+ * @extends AElement
+ * @constructor
+ */
 function Radio() {
     var thisR = this;
     this.defineEvent('change');
-    this.$input = $('input', this);
-    this.$label = $('label', this);
+    this.$input = $('input', this)
+        .on('change', this.notifyChange.bind(this));
+    this.$labels = $$('span', this);
 
-    OOP.drillProperty(this, this.$input, 'value');
-
-    this.on('click', function (event) {
-        if (!thisR.checked) {
-            if (!thisR.disabled) {
-                thisR.checked = true;
-                thisR.emit('change', event, thisR);
-            }
-        }
-    });
-
-    this.sync = this.afterAttached().then(function () {
-        thisR.checked = thisR.checked;
-    });
+    OOP.drillProperty(this, this.$input, ['value', 'checked']);
 }
 
 Radio.tag = 'radio';
 
+Radio.radioProto = _(
+    '<svg class="absol-radio-icon" width="20" height="20" version="1.1" viewBox="0 0 5.2917 5.2917"' +
+    '   xmlns="http://www.w3.org/2000/svg">' +
+    '    <g transform="translate(0 -291.71)">' +
+    '        <circle class="bound" cx="2.6458" cy="294.35" r="2.4626" style="stroke-opacity:.99497;stroke-width:.26458;" />' +
+    '        <circle class="dot" cx="2.6458" cy="294.35"  r= "0.92604" style="fill-rule:evenodd;" />' +
+    '    </g>' +
+    '</svg>'
+)
+
 Radio.render = function () {
-    return _(
-        '<div class="absol-radio">' +
-        '    <input type="radio" />' +
-        '    <svg class="absol-radio-icon absol-radio-icon-left" width="20" height="20" version="1.1" viewBox="0 0 5.2917 5.2917"' +
-        '        xmlns="http://www.w3.org/2000/svg">' +
-        '        <g transform="translate(0 -291.71)">' +
-        '            <circle class="bound" cx="2.6458" cy="294.35" r="2.4626" style="stroke-opacity:.99497;stroke-width:.26458;" />' +
-        '            <circle class="dot" cx="2.6458" cy="294.35" r= "0.92604" style="fill-rule:evenodd;" />' +
-        '        </g>' +
-        '    </svg>' +
-        '    <label style="display:none"></label>' +
-        '    <svg class="absol-radio-icon absol-radio-icon-right" width="20" height="20" version="1.1" viewBox="0 0 5.2917 5.2917"' +
-        '        xmlns="http://www.w3.org/2000/svg">' +
-        '        <g transform="translate(0 -291.71)">' +
-        '            <circle class="bound" cx="2.6458" cy="294.35" r="2.4626" style="stroke-opacity:.99497;stroke-width:.26458;" />' +
-        '            <circle class="dot" cx="2.6458" cy="294.35"  r= "0.92604" style="fill-rule:evenodd;  " />' +
-        '        </g>' +
-        '    </svg>' +
-        '</div>'
-    );
+    return _({
+        tag: 'label',
+        class: 'absol-radio',
+        child: [
+            { tag: 'input', attr: { type: 'radio' } },
+            { tag: 'span', child: { text: '' } },
+            Radio.radioProto.cloneNode(true),
+            { tag: 'span', child: { text: '' } }
+        ]
+    });
+};
+
+
+Radio.prototype.notifyChange = function () {
+    this.emit('change', { type: 'change', checked: this.checked, target: this }, this);
 };
 
 
@@ -63,31 +64,6 @@ Radio.prototype.getAllFriend = function () {
 Radio.prototype.attribute = RadioButton;
 
 Radio.property = {
-    checked: {
-        set: function (value) {
-            this.$input.checked = !!value;
-            if (this.checked) {
-                this.addClass('checked');
-                var _this = this;
-                setTimeout(function () {
-                    function finish(event) {
-                        if (!_this.checked) {
-                            _this.removeClass('checked')
-                            document.body.removeEventListener('click', finish, false);
-                        }
-                    }
-
-                    document.body.addEventListener('click', finish, false);
-                }, 100);
-            }
-            else {
-                this.removeClass('checked');
-            }
-        },
-        get: function () {
-            return this.$input.checked;
-        }
-    },
     name: {
         set: function (name) {
             this.$input.setAttribute('name', name);
@@ -100,12 +76,11 @@ Radio.property = {
     text: {
         set: function (value) {
             value = (value || '').trim();
-            this.$label.innerHTML = value;
-            if (value.length == 0) this.$label.addStyle('display', 'none');
-            else this.$label.removeStyle('display');
+            this.$labels[0].firstChild.data = value;
+            this.$labels[1].firstChild.data = value;
         },
         get: function () {
-            return this.$label.innerHTML;
+            return this.$labels[0].firstChild.data;
         }
     },
     disabled: {
