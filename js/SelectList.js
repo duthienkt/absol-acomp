adcb03300868cc977a2f68b0eb96881019e1d110
duthienkt <blueskythien2010@live.com>
2020-09-07 00:31:32 +0700

    * fix overflow list

--- js/SelectList.js
+++ js/SelectList.js
@@ -134,7 +134,7 @@ export function measureListSize(items) {
     var textWidth = measureMaxTextWidth(items);
     var width = 8 + textWidth + 34 + 16 + 2;//padding, margin
     if (descWidth > 0) {
-        width += +descWidth + 14 + 12 + 20;
+        width += +descWidth + 14 + 12 + 25;
     }
     var height = measureListHeight(items);
     return {
