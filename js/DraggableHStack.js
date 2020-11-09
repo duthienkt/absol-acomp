a2b89432ac5962f018f464bc951f5d00830cec32
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-11-09 19:04:33 +0700

    - DraggableHStack with better solution

--- js/DraggableHStack.js
+++ js/DraggableHStack.js
@@ -1,208 +1,205 @@
 import ACore from "../ACore";
 import '../css/draggablestack.css';
 import DraggableVStack from "./DraggableVStack";
+import Vec2 from "absol/src/Math/Vec2";
+import Rectangle from "absol/src/Math/Rectangle";
+import PositionTracker from "./PositionTracker";
 
 var _ = ACore._;
 var $ = ACore.$;
 
 /***
- * @extends DraggableVStack
+ * @augments DraggableVStack
+ * @augments PositionTracker
  * @constructor
  */
 function DraggableHStack() {
+    this._dragData = null;
     this.$cloneContainer = _('.absol-draggable-stack-clone-container');
-    this.$destLine = _('.absol-draggable-stack-dest-line');
     this.on('predrag', this.eventHandler.predrag)
         .on('dragstart', this.eventHandler.dragstart)
         .on('drag', this.eventHandler.drag)
         .on('dragend', this.eventHandler.dragend);
+    _({ elt: this, tag: PositionTracker })
+        .on('positionchange', this._updateDraggingPosition.bind(this));
 }
 
 
-
 DraggableHStack.tag = "DraggableHStack".toLowerCase();
-DraggableHStack.render = function(){
+DraggableHStack.render = function () {
     return _({
-        tag:'hanger',
+        tag: 'hanger',
         extendEvent: 'change',
         class: ['absol-draggable-stack', 'absol-draggable-hstack']
     });
 };
 
+/***
+ *
+ * @param {Rectangle} rect
+ * @returns {number}
+ * @private
+ */
+DraggableHStack.prototype._findDropIdx = function (rect) {
+    if (!this._dragData) return -1;
+    var centerX = rect.x + rect.width / 2;
+    var currentBound = Rectangle.fromClientRect(this.getBoundingClientRect());
+    var nearestDistance = Math.abs(centerX - currentBound.width);//end of stack
+    var cDist;
+    var nearestIndex = this._dragData.childBounds.length;
+    var childX;
+    for (var i = 0; i < this._dragData.childBounds.length; ++i) {
+        childX = this._dragData.childBounds[i].x + currentBound.x - this._dragData.bound.x;
+        cDist = Math.abs(centerX - childX);
+        if (cDist < nearestDistance) {
+            nearestDistance = cDist;
+            nearestIndex = i;
+        }
+    }
+    return nearestIndex;
+};
 
+/***
+ *
+ * @type {DraggableHStack|{}}
+ */
 DraggableHStack.eventHandler = {};
 
 DraggableHStack.eventHandler.predrag = DraggableVStack.eventHandler.predrag;
 
-DraggableHStack.eventHandler.dragstart = function(event){
-    var self = this;
-    this._mouseClientX = event.clientX;
-    this._mouseClientY = event.clientY;
-
-    var bound = this.getBoundingClientRect();
-    var element = this._findDirectChild(event.target);
-    element.classList.add('dragging');
-    this.$draggingElt = element;
-    this._draggingEltIndex = 0;
-    this._childrentInfo = Array.prototype.map.call(this.childNodes, function (child, index) {
-        var childBound = child.getBoundingClientRect();
-        if (child == element) self._draggingEltIndex = index;
-        return {
-            index: index,
-            elt: child,
-            bound: childBound,
-            left: childBound.left - bound.left,
-        }
+DraggableHStack.eventHandler.dragstart = function (event) {
+    this.addClass('as-has-dragging');
+    this.startTrackPosition();
+    var mousePos = new Vec2(event.clientX, event.clientY);
+    var elt = this._findDirectChild(event.target);
+    var childBounds = Array.prototype.map.call(this.childNodes, function (e) {
+        return Rectangle.fromClientRect(e.getBoundingClientRect());
     });
+    var eltIdx = Array.prototype.indexOf.call(this.childNodes, elt);
+    var eltBound = childBounds[eltIdx];
+    var eltMouseOffset = mousePos.sub(new Vec2(eltBound.x, eltBound.y));
 
-    this.$cloneContainer.addTo(document.body);
-    this.$destLine.addTo(this);
 
-    var containerBound = element.getBoundingClientRect();
-    this._initBound = bound;
-    this._currentBound = bound;
+    this._dragData = {
+        mouseStartPos: mousePos,
+        mousePos: mousePos,
+        bound: Rectangle.fromClientRect(this.getBoundingClientRect()),
+        childBounds: childBounds,
+        elt: elt,
+        eltIdx: eltIdx,
+        eltBound: eltBound,
+        eltMouseOffset: eltMouseOffset
+    };
 
-    this._initTop = bound.top;
-    this._initLeft =  bound.left;
-    this._crLeft = this._initLeft;
-    this._initWidth = containerBound.width;
-    this._pressX = event.clientX - containerBound.left;
-    this._pressY = event.clientY - containerBound.top;
     this.$cloneContainer.addStyle({
-        top: this._initTop + 'px',
-        left: this._initLeft + 'px',
-        width: this._initWidth + 'px'
-    }).addChild($(element.cloneNode(true)).addStyle({'width': '100%', 'box-sizing':'border-box'}));
-
-    this.$destLine.addStyle('left', this._initLeft + 'px');
-
-    this.$scrollTrackElements = [];
-    var trackElt = this.parentElement;
-    while (trackElt) {
-        if (trackElt.addEventListener)
-            trackElt.addEventListener('scroll', this.eventHandler.scroll, false);
-        else
-            trackElt.attachEvent('onscroll', this.eventHandler.scroll, false);
-
-        this.$scrollTrackElements.push(trackElt);
-        trackElt = trackElt.parentElement;
-    }
-    if (document.addEventListener) {
-        document.addEventListener('scroll', this.eventHandler.scroll, false);
-    }
-    else {
-        document.attachEvent('onscroll', this.eventHandler.scroll, false);
-    }
-    this.$scrollTrackElements.push(document);
+        left: eltBound.x + 'px',
+        top: eltBound.y + 'px',
+        width: eltBound.width + 'px',
+        height: eltBound.height + 'px',
+    }).clearChild()
+        .addChild($(elt.cloneNode(true)))
+        .addTo(document.body);
+    elt.addClass('as-dragging');
     this._updateDraggingPosition();
 };
 
 
 DraggableHStack.eventHandler.drag = function (event) {
     event.preventDefault();
-    //save mouse position 
-    this._mouseClientX = event.clientX;
-    this._mouseClientY = event.clientY;
+    this._dragData.mousePos = new Vec2(event.clientX, event.clientY);
     this._updateDraggingPosition();
 };
 
 
 DraggableHStack.eventHandler.dragend = function (event) {
-    var self = this;
-
-    this.$scrollTrackElements.forEach(function (e) {
-        if (e.removeEventListener)
-            e.removeEventListener('scroll', self.eventHandler.scroll, false);
-        else
-            e.dettachEvent('onscroll', self.eventHandler.scroll, false);
-    });
-    if ((this._draggingEltIndex == this._childrentInfo.length - 1 && !this._destRecord)
-        || (this._destRecord && (this._destRecord.index == this._draggingEltIndex || this._destRecord.index == this._draggingEltIndex + 1))) {
-        //nothing to change, view animation
-        this.$cloneContainer.addClass('home-going');
+    var thisS = this;
+    this.stopTrackPosition();
+    this.removeClass('as-has-dragging');
+    this._dragData.elt.removeClass('as-dragging');
+    if (this._dragData.destIdx == this._dragData.eltIdx || this._dragData.destIdx == this._dragData.eltIdx + 1) {
+        this.$cloneContainer.addClass('as-home-going');
+        this._updateDraggingPosition();
         setTimeout(function () {
-            self.$cloneContainer.addStyle({
-                left: self._initLeft + 'px'
-            });
-        }, 0);
-        setTimeout(function () {
-            self.$cloneContainer.clearChild().removeClass('home-going').remove();
-            self.$destLine.removeStyle({ left: '' }).remove();
-            self.$draggingElt.classList.remove('dragging');
-            self.$draggingElt = undefined;
-        }, 200);
+            thisS.$cloneContainer.selfRemove()
+                .removeClass('as-home-going');
+        }, 100);
     }
     else {
-        this.$draggingElt.remove();
-        this.$destLine.removeStyle({ top: '' }).remove();
-        this.$cloneContainer.clearChild().remove();
-        this.$draggingElt.classList.remove('dragging');
-        if (this._destRecord) {
-            this.addChildBefore(this.$draggingElt, this._destRecord.elt);
+        if (this._dragData.destIdx === this._dragData.childBounds.length) {
+            this._dragData.elt.remove();
+            this.addChild(this._dragData.elt);
             this.emit('change', {
-                type: 'change', target: this, action: "BEFORE", at: this._destRecord.elt,
-                sourceIndex: this._draggingEltIndex, destIndex: this._destIndex, elt: this.$draggingElt
+                type: 'change',
+                elt: this._dragData.elt,
+                sourceIndex: this._dragData.eltIdx,
+                destIndex: this._dragData.childBounds.length,
+                oldIdx: this._dragData.eltIdx,
+                newIdx: this._dragData.childBounds.length - 1,
+                desc: "Move element to end of stack."
             }, this);
         }
         else {
-            this.addChild(this.$draggingElt);
-            this.emit('change', { type: 'change', target: this, action: "END", elt: this.$draggingElt, sourceIndex: this._draggingEltIndex }, this);
+            var beforeElt = this.childNodes[this._dragData.destIdx];
+            this._dragData.elt.remove();
+            this.addChildBefore(this._dragData.elt, beforeElt);
+            this.emit('change', {
+                type: 'change',
+                elt: this._dragData.elt,
+                sourceIndex: this._dragData.eltIdx,
+                destIndex: this._dragData.destIdx,
+                oldIdx: this._dragData.eltIdx,
+                newIdx: this._dragData.destIdx > this._dragData.eltIdx ? this._dragData.destIdx - 1 : this._dragData.destIdx,
+                desc: "Move element to before  this.childNodes[" + this._dragData.destIdx + "]"
+            }, this);
         }
+        this.$cloneContainer.selfRemove()
+            .removeClass('as-home-going');
     }
+
+    this.removeClass('as-no-change');
 };
 
 
 DraggableHStack.prototype._findDragzone = DraggableVStack.prototype._findDragzone;
 DraggableHStack.prototype._findDirectChild = DraggableVStack.prototype._findDirectChild;
 
-DraggableHStack.eventHandler.scroll = function (event) {
-    this._updateDraggingPosition();
-};
-var count = 0;
+
 DraggableHStack.prototype._updateDraggingPosition = function () {
-    // console.log(count++);
-    //update cloneContainer
     var bound = this.getBoundingClientRect();
-    //style top of cloneContainer
-    this._crLeft = this._mouseClientX - bound.left - this._pressX;
+    var x, y;
+    if (this.$cloneContainer.containsClass('as-home-going')) {
+        x = this._dragData.eltBound.x - this._dragData.bound.x + bound.left;
+        y = this._dragData.eltBound.y - this._dragData.bound.y + bound.top;
+    }
+    else {
+        y = bound.top + (this._dragData.eltBound.y - this._dragData.bound.y);
+        var newPos = this._dragData.mousePos.sub(this._dragData.eltMouseOffset);
+        x = newPos.x;
+    }
     this.$cloneContainer.addStyle({
-        left: this._crLeft + 'px'
+        left: x + 'px',
+        top: y + 'px'
     });
-
-    //update destLine
-    var centerX = this._crLeft + this._initWidth / 2;
-    var nearestRecord;
-    var nearestDistance = Math.abs(centerX - bound.width);//end of stack
-    var cDist;
-    var nearestIndex = this._childrentInfo.length;
-    for (var i = 0; i < this._childrentInfo.length; ++i) {
-        cDist = Math.abs(centerX - this._childrentInfo[i].left);
-        if (cDist < nearestDistance) {
-            nearestRecord = this._childrentInfo[i];
-            nearestDistance = cDist;
-            nearestIndex = i;
-        }
-    }
-
-
-    if (nearestRecord) {
-        this.$destLine.addStyle('left', nearestRecord.left + 'px');
+    var cBound = this._dragData.eltBound.clone();
+    cBound.x = x;
+    cBound.y = y;
+    var destIdx = this._findDropIdx(cBound);
+    this._dragData.destIdx = destIdx;
+    var destX;
+    if (destIdx >= this._dragData.childBounds.length) {
+        var lastRect = this._dragData.childBounds[this._dragData.childBounds.length - 1];
+        destX = lastRect.x + lastRect.width - this._dragData.bound.x;
     }
     else {
-        var lastRecord = this._childrentInfo[this._childrentInfo.length - 1];
-        this.$destLine.addStyle('left', lastRecord.left + lastRecord.bound.width + 'px');
+        destX = this._dragData.childBounds[destIdx].x - this._dragData.bound.x;
     }
-
-    if (nearestIndex == this._draggingEltIndex || nearestIndex == this._draggingEltIndex + 1) {
-        this.$destLine.addStyle('visibility', 'hidden');
+    this.addStyle('--dest-x', destX + 'px');
+    if (destIdx == this._dragData.eltIdx || destIdx == this._dragData.eltIdx + 1) {
+        this.addClass('as-no-change');
     }
     else {
-        this.$destLine.removeStyle('visibility', 'hidden');
-
+        this.removeClass('as-no-change');
     }
-    this._destRecord = nearestRecord;
-    this._destIndex = nearestIndex;
-    // setTimeout(this._autoScrollParentIfNeed.bind(this, 10), 33);
 };
 
 
