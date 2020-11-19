fcc54039610d2c4a53d3ba03a12137f259436ba9
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-11-19 10:56:56 +0700

    - export

--- ACore.js
+++ ACore.js
@@ -1,6 +1,7 @@
 import Dom from "absol/src/HTML5/Dom";
 
 var ACore = new Dom({ creator: Object.assign({}, Dom.ShareInstance.creator) });
-
+export var _ = ACore._;
+export var $ = ACore.$;
 
 export default ACore;
