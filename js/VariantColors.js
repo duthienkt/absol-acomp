4928dee3a2c83445bbdd580e9847f770a6512fb6
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-23 11:02:12 +0700

    - more variant colors

--- js/VariantColors.js
+++ js/VariantColors.js
@@ -19,8 +19,21 @@ function VariantColor() {
         light: "#f8f9fa",
         dark: "#343a40",
         link: "#007bff",
-        note: '#1e1ec8'
+        note: '#ffff88'
     };
+    this.mediumContract = {
+        primary: '#e2edd5',
+        secondary: '#fca75b',
+        success: '#fce8e8',
+        info: '#fcf5e8',
+        warning: '#5e5a75',
+        error: '#e0dfce',
+        danger: "#e5e8d5",
+        light: '#7a5b3c',
+        dark: "#bf7d3b",
+        link: "#dde8c9",
+        note: '#1e1ec8'
+    }
     Dom.documentReady.then(this.loadFromCss.bind(this));
 }
 
@@ -32,6 +45,11 @@ VariantColor.prototype.loadFromCss = function () {
     this.base = this.keys.reduce(function (ac, name) {
         var style = window.getComputedStyle(document.body);
         ac[name] = style.getPropertyValue('--variant-color-' + name);
+        return ac;
+    }, {});
+    this.mediumContract = this.keys.reduce(function (ac, name) {
+        var style = window.getComputedStyle(document.body);
+        ac[name] = style.getPropertyValue('--variant-medium-contract-color-' + name);
         return ac;
     }, {});
 };
