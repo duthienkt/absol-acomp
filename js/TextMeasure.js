830ff311cfdbc76bd7e1a38c3001e75afb277ba2
Pham Quoc Du Thien <blueskythien2010@live.com>
2021-01-08 11:29:20 +0700

    + text measure

---
+++ js/TextMeasure.js
@@ -0,0 +1,29 @@
+import sText from './sample.tpl';
+import {measureText} from "./utils";
+
+// var charList = Object.keys(sText.split('').reduce(function (ac, cr){
+//     ac[cr] = true;
+//     return ac;
+// }));
+//
+// console.log(charList)
+
+
+function TextMeasure() {
+    this._makeSizeData();
+}
+
+
+TextMeasure.prototype.supportFont = ['Times New Roman', 'Arial'];
+TextMeasure.prototype.characterList  = []
+
+TextMeasure.prototype._makeSizeData = function () {
+
+};
+
+TextMeasure.prototype._makeFontSize = function (fontName){
+    measureText()
+}
+
+
+export default new TextMeasure;
