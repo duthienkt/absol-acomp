d075c573b4524840e8ff614f4928b7f81acf24ed
duthienkt <blueskythien2010@live.com>
2021-02-22 14:36:52 +0700

    - any type of message

--- js/Snackbar.js
+++ js/Snackbar.js
@@ -2,6 +2,7 @@ import '../css/snackbar.css';
 import ACore from "../ACore";
 import OOP from "absol/src/HTML5/OOP";
 import AElement from "absol/src/HTML5/AElement";
+import {isDomNode} from "absol/src/HTML5/Dom";
 
 var _ = ACore._;
 var $ = ACore.$;
@@ -13,18 +14,59 @@ var t = document.createElement;
  * @constructor
  */
 function SnackBar() {
-    OOP.drillProperty(this, this.firstChild, 'message', 'data');
-
+    // OOP.drillProperty(this, this.firstChild, 'message', 'data');
+    /***
+     *
+     * @type {*}
+     */
+    this._message = null;
+    this.message = null;
 }
 
 SnackBar.tag = 'SnackBar'.toLowerCase();
 
 SnackBar.render = function () {
     return _({
-        class: 'as-snackbar',
-        child: { text: '' }
+        class: 'as-snackbar'
     })
 };
+
+SnackBar.property = {};
+
+SnackBar.property.message = {
+    set: function (value) {
+        this._message = value;
+        if (value instanceof Array) {
+            this.clearChild();
+            this.addChild(value.map(function (item) {
+                return _(item);
+            }));
+        }
+        else if (typeof value === 'string') {
+            this.innerHTML = value;
+        }
+        else if (isDomNode(value)) {
+            this.clearChild()
+                .addChild(value);
+        }
+        else if (value && typeof value === 'object' &&
+            (value.text
+                || value.props || value.tag || value.attr
+                || value.style || value.child
+                || (value.child && value.child instanceof Array))) {
+            this.clearChild()
+                .addChild(_(value));
+        }
+        else {
+            this.clearChild();
+            this._message = null;
+        }
+    },
+    get: function () {
+        return this._message;
+    }
+}
+
 
 ACore.install(SnackBar);
 
@@ -37,7 +79,7 @@ SnackBar.show = function (message) {
     }
     if (!SnackBar.$instance.parentElement) document.body.appendChild(SnackBar.$instance);
     SnackBar.$instance.removeClass('as-hiding');
-    SnackBar._removeTimeout = setTimeout(function (){
+    SnackBar._removeTimeout = setTimeout(function () {
         SnackBar.$instance.addClass('as-show');
         SnackBar.$instance.message = message;
         SnackBar._removeTimeout = setTimeout(function () {
