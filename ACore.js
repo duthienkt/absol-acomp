025253bcfddc57d579dba87503947fcf406c3724
duthienkt <blueskythien2010@live.com>
2021-05-18 21:09:02 +0700

    - export $$

--- ACore.js
+++ ACore.js
@@ -3,5 +3,6 @@ import Dom from "absol/src/HTML5/Dom";
 var ACore = new Dom({ creator: Object.assign({}, Dom.ShareInstance.creator) });
 export var _ = ACore._;
 export var $ = ACore.$;
+export var $$ = ACore.$$;
 
 export default ACore;
