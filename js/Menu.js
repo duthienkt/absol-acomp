a57555263f5a94482d5894d17061c0848939a204
duthienkt <blueskythien2010@live.com>
2021-08-05 14:46:08 +0700

    - remove overflow

--- js/Menu.js
+++ js/Menu.js
@@ -720,7 +720,7 @@ VRootMenu.tag = 'VRootMenu'.toLowerCase();
 
 VRootMenu.render = function () {
     return _({
-        class: ['as-v-root-menu', 'as-bscroller'],
+        class: ['as-v-root-menu'],
         extendEvent: ['press', 'enter', 'activetab', 'cancel']
     });
 };
