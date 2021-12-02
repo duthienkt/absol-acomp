f4986f9a47c9a2c7c535269b1d1757102cf4cf5c
duthienkt <blueskythien2010@live.com>
2021-12-02 22:44:33 +0700

    - demo

---
+++ demo/demo_tree_list_with_icon.js
@@ -0,0 +1,157 @@
+module.exports = [
+    {
+        value: 9,
+        text: "Công ty 2h",
+        icon: {
+            tag: "span",
+            class: [
+                "mdi",
+                "mdi-folder-account"
+            ]
+        },
+        items: [
+            {
+                value: 19,
+                text: "Bộ phận A211",
+                icon: {
+                    tag: "span",
+                    class: [
+                        "mdi",
+                        "mdi-folder-account"
+                    ]
+                },
+                items: [
+                    {
+                        value: 18,
+                        text: "abc",
+                        icon: {
+                            tag: "span",
+                            class: [
+                                "mdi",
+                                "mdi-folder-account"
+                            ]
+                        },
+                        items: [],
+                        sortText: "abc"
+                    },
+                    {
+                        value: -17,
+                        text: "Admin",
+                        icon: {
+                            tag: "span",
+                            class: [
+                                "mdi",
+                                "mdi-account"
+                            ]
+                        },
+                        sortText: "admin"
+                    },
+                    {
+                        value: -18,
+                        text: "dat",
+                        icon: {
+                            tag: "span",
+                            class: [
+                                "mdi",
+                                "mdi-account"
+                            ]
+                        },
+                        sortText: "dat"
+                    },
+                    {
+                        value: -19,
+                        text: "hieu",
+                        icon: {
+                            tag: "span",
+                            class: [
+                                "mdi",
+                                "mdi-account"
+                            ]
+                        },
+                        sortText: "hieu"
+                    },
+                    {
+                        value: -21,
+                        text: "Trần Thị Thanh Yên",
+                        icon: {
+                            tag: "span",
+                            class: [
+                                "mdi",
+                                "mdi-account"
+                            ]
+                        },
+                        sortText: "tran thi thanh yen"
+                    }
+                ],
+                sortText: "bo phan a211"
+            },
+            {
+                value: 15,
+                text: "ssssssssssssss",
+                icon: {
+                    tag: "span",
+                    class: [
+                        "mdi",
+                        "mdi-folder-account"
+                    ]
+                },
+                items: [
+                    {
+                        value: -5,
+                        text: "a2",
+                        icon: {
+                            tag: "span",
+                            class: [
+                                "mdi",
+                                "mdi-account"
+                            ]
+                        },
+                        sortText: "a2"
+                    }
+                ],
+                sortText: "ssssssssssssss"
+            }
+        ],
+        sortText: "cong ty 2h"
+    },
+    {
+        value: 16,
+        text: "sssssssss",
+        icon: {
+            tag: "span",
+            class: [
+                "mdi",
+                "mdi-folder-account"
+            ]
+        },
+        items: [
+            {
+                value: 17,
+                text: "123",
+                icon: {
+                    tag: "span",
+                    class: [
+                        "mdi",
+                        "mdi-folder-account"
+                    ]
+                },
+                items: [
+                    {
+                        value: -6,
+                        text: "b1",
+                        icon: {
+                            tag: "span",
+                            class: [
+                                "mdi",
+                                "mdi-account"
+                            ]
+                        },
+                        sortText: "b1"
+                    }
+                ],
+                sortText: "123"
+            }
+        ],
+        sortText: "sssssssss"
+    }
+];
