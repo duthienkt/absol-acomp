c55159b2301b3202ec380bdd14688c2d5ec780c1
duthienkt <blueskythien2010@live.com>
2022-01-04 22:51:05 +0700

    - upgrade QuickMenu

--- js/QuickMenu.js
+++ js/QuickMenu.js
@@ -1,9 +1,10 @@
 import ACore from "../ACore";
-import Dom, {getScreenSize, traceOutBoundingClientRect} from "absol/src/HTML5/Dom";
+import Dom, { getScreenSize, traceOutBoundingClientRect } from "absol/src/HTML5/Dom";
 import Rectangle from "absol/src/Math/Rectangle";
 import BrowserDetector from "absol/src/Detector/BrowserDetector";
 import './Menu';
-import {cleanMenuItemProperty} from "./utils";
+import { cleanMenuItemProperty } from "./utils";
+import Follower from "./Follower";
 
 var isMobile = BrowserDetector.isMobile;
 var _ = ACore._;
@@ -33,207 +34,83 @@ QuickMenu.render = function () {
 
 ACore.install(QuickMenu);
 QuickMenu.PRIORITY_ANCHORS = [0, 3, 7, 4, 1, 2, 6, 5];
-QuickMenu.DEFAULT_ANCHOR = 0;
 
 QuickMenu.$elt = _('quickmenu');
-QuickMenu.$element = undefined;
-QuickMenu.$anchor = _('.absol-context-menu-anchor.as-context-menu-ctn');
-QuickMenu.$anchor.addChild(QuickMenu.$elt);
+/***
+ *
+ * @type {Follower}
+ */
+QuickMenu.$follower = _({
+    tag: Follower.tag,
+    class: 'absol-context-menu-anchor',
+    child: QuickMenu.$elt,
+    on: {
+        preupdateposition: function () {
+            var bound = this.getBoundingClientRect();
+            var outBound = traceOutBoundingClientRect(this);
+            if (bound.bottom < outBound.top || bound.top > outBound.bottom || bound.right < outBound.left || bound.left > outBound.right) {
+                QuickMenu.close(QuickMenu._session);
+            }
 
-QuickMenu._acceptAnchors = 0;
-QuickMenu._previewAnchor = QuickMenu.DEFAULT_ANCHOR;
+        }
+    }
+});
+
+
 QuickMenu._session = Math.random() * 10000000000 >> 0;
 QuickMenu._menuListener = undefined;
-QuickMenu._scrollOutListener = undefined;
 
 QuickMenu.$elt.on('press', function (event) {
     if (QuickMenu._menuListener) QuickMenu._menuListener(cleanMenuItemProperty(event.menuItem));
 });
 
 
-QuickMenu.updatePosition = function () {
-    if (!QuickMenu.$element) return;
-    var menu = QuickMenu.$elt;
-    var anchor = QuickMenu.$anchor;
-    var eBound = QuickMenu.$element.getBoundingClientRect();
-    var outBound = traceOutBoundingClientRect(QuickMenu.$element);
-
-    if (eBound.bottom < outBound.top || eBound.left > outBound.right || eBound.top > outBound.bottom
-        || eBound.right < outBound.left) {
-        QuickMenu._scrollOutListener && QuickMenu._scrollOutListener();
-    }
-
-    var qBound = menu.getBoundingClientRect();
-    outBound = getScreenSize();
-    outBound.left = 0;
-    outBound.right = outBound.width;
-    outBound.top = 0;
-    outBound.bottom = outBound.height;
-    //padding
-    outBound.left += 2;
-    outBound.top += 2;
-    outBound.bottom -= 2;
-    outBound.right -= 2;
-    outBound.height -= 4;
-    outBound.width -= 4;
-
-    var getPos = function (anchor) {
-        anchor = anchor % 8;
-        var x = 0;
-        var y = 0;
-        if (anchor == 0 || anchor == 3) {
-            y = eBound.top;
-        }
-
-        if (anchor == 0 || anchor == 7) {
-            x = eBound.right;
-        }
-
-        if (anchor == 1 || anchor == 6) {
-            x = eBound.left;
-        }
-
-        if (anchor == 1 || anchor == 2) {
-            y = eBound.bottom;
-        }
-
-        if (anchor == 2 || anchor == 5) {
-            x = eBound.right - qBound.width;
-        }
-
-        if (anchor == 3 || anchor == 4) {
-            x = eBound.left - qBound.width;
-        }
-
-        if (anchor == 4 || anchor == 7) {
-            y = eBound.bottom - qBound.height;
-        }
-
-        if (anchor == 5 || anchor == 6) {
-            y = eBound.top - qBound.height;
-        }
-        return { x: x, y: y };
-    };
-
-    var pos;
-
-    var bestSquare = -1;
-    var bestRect;
-    var priority = ((typeof QuickMenu._previewAnchor === 'number') ? [QuickMenu._previewAnchor] : []).concat(QuickMenu._acceptAnchors);
-    if (priority.length) {
-
-        var cAnchor;
-        var outRect = new Rectangle(outBound.left, outBound.top, outBound.width, outBound.height);
-
-        var menuRect;
-        var viewSquare;
-        var cPos;
-        for (var i = 0; i < priority.length; ++i) {
-            cAnchor = priority[i];
-            cPos = getPos(cAnchor);
-            menuRect = new Rectangle(cPos.x, cPos.y, qBound.width, qBound.height);
-            viewSquare = outRect.collapsedSquare(menuRect);
-
-            if (viewSquare - 0.01 > bestSquare) {
-                bestSquare = viewSquare;
-                pos = cPos;
-                QuickMenu._previewAnchor = cAnchor;
-                bestRect = outRect.collapsedRect(menuRect);
-            }
-        }
-
-        anchor.addStyle({
-            left: pos.x + 'px',
-            top: pos.y + 'px'
-        });
-    }
-    else {
-        anchor.removeStyle({
-            left: null,
-            top: null
-        });
-    }
-
-};
-
-QuickMenu._scrollEventHandler = QuickMenu.updatePosition.bind(QuickMenu);
-QuickMenu.$scrollTrackElements = [];
-
-
 QuickMenu.show = function (element, menuProps, anchor, menuListener, darkTheme) {
     var menuElt = QuickMenu.$elt;
-    var anchorElt = QuickMenu.$anchor;
-    //untrack all element
-    QuickMenu.$scrollTrackElements.forEach(function (e) {
-        if (e.removeEventListener)
-            e.removeEventListener('scroll', QuickMenu._scrollEventHandler, false);
-        else
-            e.dettachEvent('onscroll', QuickMenu._scrollEventHandler, false);
+    var followerElt = QuickMenu.$follower;
+    var menuAnchors = [];
 
-    });
-    QuickMenu.$scrollTrackElements = [];
     if (typeof anchor == 'number') {
-        QuickMenu._acceptAnchors = [anchor];
+        menuAnchors = [anchor];
     }
     else if (anchor instanceof Array) {
-        QuickMenu._acceptAnchors = anchor;
+        menuAnchors = anchor;
     }
     else if (anchor === 'modal') {
-        QuickMenu._acceptAnchors = [];
-
+        menuAnchors = [];
     }
     else {
         QuickMenu._acceptAnchors = QuickMenu.PRIORITY_ANCHORS;
-
     }
+
     if (anchor === 'modal') {
-        QuickMenu.$anchor.addClass('as-anchor-modal');
+        followerElt.addClass('as-anchor-modal');
     }
     else {
-        QuickMenu.$anchor.removeClass('as-anchor-modal');
+        followerElt.removeClass('as-anchor-modal');
     }
 
 
-    QuickMenu._previewAnchor = QuickMenu._acceptAnchors[0];
-
     QuickMenu._session = Math.random() * 10000000000 >> 0;
-    QuickMenu.$anchor.addTo(document.body)
-    Dom.addToResizeSystem(QuickMenu.$elt);
-    QuickMenu.$elt.updateSize = QuickMenu.updatePosition.bind(QuickMenu);
+    followerElt.addTo(document.body);
+    Object.assign(menuElt, menuProps);
+    followerElt.anchors = menuAnchors;
+    followerElt.followTarget = element;
     QuickMenu.$element = element;
     QuickMenu.$element.classList.add('as-quick-menu-attached');
     QuickMenu._menuListener = menuListener;
 
-    Object.assign(menuElt, menuProps);
-    if (darkTheme) anchorElt.addClass('dark');
-    else anchorElt.removeClass('dark');
-    menuElt.addStyle('visibility', 'hidden');//for prevent size change blink
-    QuickMenu.$anchor.addClass('absol-active');
 
-    QuickMenu.updatePosition();
+    if (darkTheme) followerElt.addClass('dark');
+    else followerElt.removeClass('dark');
+    menuElt.addStyle('visibility', 'hidden');//for prevent size change blink
+    followerElt.addClass('absol-active');
+
     setTimeout(function () {
+        followerElt.updatePosition();
         menuElt.addStyle('visibility', 'visible');
     }, isMobile ? 33 : 2);
 
-    //track element
-    var trackElt = element.parentElement;
-    while (trackElt) {
-        // trackElt.addEventListener('scroll', QuickMenu._scrollEventHandler, false);
-        if (trackElt.addEventListener)
-            trackElt.addEventListener('scroll', QuickMenu._scrollEventHandler, false);
-        else
-            trackElt.attachEvent('onscroll', QuickMenu._scrollEventHandler, false);
-
-        QuickMenu.$scrollTrackElements.push(trackElt);
-        trackElt = trackElt.parentElement;
-    }
-    if (document.addEventListener) {
-        document.addEventListener('scroll', QuickMenu._scrollEventHandler, false);
-    }
-    else {
-        document.attachEvent('onscroll', QuickMenu._scrollEventHandler, false);
-    }
-    QuickMenu.$scrollTrackElements.push(document);
 
     return QuickMenu._session;
 };
@@ -245,21 +122,9 @@ QuickMenu.close = function (session) {
         QuickMenu.$element.classList.remove('as-quick-menu-attached');
     QuickMenu.$element = undefined;
     QuickMenu._menuListener = undefined;
-    QuickMenu._previewAnchor = QuickMenu.DEFAULT_ANCHOR;
 
-    //untrack all element
-    QuickMenu.$scrollTrackElements.forEach(function (e) {
-        e.removeEventListener('scroll', QuickMenu._scrollEventHandler, false);
-    });
-    QuickMenu.$scrollTrackElements = [];
-    var menuElt = QuickMenu.$elt;
-    menuElt.removeStyle('visibility');//for prevent size change blink
-    menuElt.removeStyle({
-        left: true,
-        top: true
-    });
-    QuickMenu.$anchor.removeClass('absol-active');
-    QuickMenu.$anchor.remove();
+    QuickMenu.$follower.removeClass('absol-active');
+    QuickMenu.$follower.remove();
 };
 
 
@@ -277,15 +142,13 @@ QuickMenu.showWhenClick = function (element, menuProps, anchor, menuListener, da
         if (QuickMenu._session === res.currentSession) return;
         res.currentSession = QuickMenu.show(res.element, res.menuProps, res.anchor, res.menuListener, res.darkTheme);
         var finish = function () {
-            document.body.removeEventListener('click', finish, false);
+            document.removeEventListener('click', finish, false);
             QuickMenu.close(res.currentSession);
             res.currentSession = undefined;
-            if (QuickMenu._scrollOutListener === finish) QuickMenu._scrollOutListener = undefined;
         };
-        QuickMenu._scrollOutListener = finish;
 
         setTimeout(function () {
-            document.body.addEventListener('click', finish, false);
+            document.addEventListener('click', finish, false);
         }, 10)
     };
 
@@ -341,9 +204,7 @@ QuickMenu.toggleWhenClick = function (trigger, adaptor) {
             QuickMenu.close(res.currentSession);
             if (adaptor.onClose) adaptor.onClose();
             res.currentSession = undefined;
-            if (QuickMenu._scrollOutListener === finish) QuickMenu._scrollOutListener = undefined;
         };
-        QuickMenu._scrollOutListener = finish;
 
         setTimeout(function () {
             document.body.addEventListener('click', finish, false);
