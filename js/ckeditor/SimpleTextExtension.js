67f8e1e6286cb05c40b8d7f15f53d34f64c5849d
duthienkt <blueskythien2010@live.com>
2022-01-14 15:12:15 +0700

    + SimpleTextExtension

---
+++ js/ckeditor/SimpleTextExtension.js
@@ -0,0 +1,21 @@
+import { _ } from "../../ACore";
+
+var name = 'simple_text';
+
+var parserDiv = _('div');
+
+function explicit(data, placeHolderElt) {
+    parserDiv.innerHTML = data.replace(/&nbsp;/g, ' ');
+    return parserDiv.innerText;
+}
+
+function implicit(data, placeHolderElt) {
+    return data.replace(/</g, '&lt;').replace(/</g, '&gt;').replace(/"/g, '&quot;').replace(/&/g, '&amp;').replace(/[\r\n]/g, '');
+}
+
+
+export default {
+    name: name,
+    implicit: implicit,
+    explicit: explicit,
+}
