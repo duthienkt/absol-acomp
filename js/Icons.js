cc1fabd5108c946dab51ff5e678d65fba29db074
duthienkt <blueskythien2010@live.com>
2022-04-20 10:58:56 +0700

    - icon

--- js/Icons.js
+++ js/Icons.js
@@ -1,9 +1,20 @@
 import ACore from "../ACore";
 import SpinnerIcoText from '../assets/icon/spinner.tpl';
-export  function SpinnerIco(){
+import MdiStoreMarkerOutlineText from '../assets/icon/mdi_store_marker_outline.tpl';
+import '../css/icons.css';
+
+export function SpinnerIco() {
     return ACore._(SpinnerIcoText);
 }
 
 SpinnerIco.tag = 'SpinnerIco'.toLowerCase();
 
 ACore.install(SpinnerIco);
+
+export function MdiStoreMarkerOutline() {
+    return ACore._(MdiStoreMarkerOutlineText);
+}
+
+MdiStoreMarkerOutline.tag = 'mdi-store-marker-outline';
+
+ACore.install(MdiStoreMarkerOutline);
