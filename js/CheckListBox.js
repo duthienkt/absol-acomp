6076c537444699f629f80e1f074064980773c313
duthienkt <blueskythien2010@live.com>
2022-04-20 10:58:04 +0700

    * mobile

--- js/CheckListBox.js
+++ js/CheckListBox.js
@@ -120,8 +120,9 @@ CheckListBox.prototype._initFooter = function () {
         .on('change', this.eventHandler.checkAllChange);
     this.$cancelBtn = $('.as-select-list-box-cancel-btn', this)
         .on('click', this.eventHandler.clickCancelBtn);
-    this.$closeBtn = $('.as-select-list-box-close-btn', this)
-        .on('click', this.eventHandler.clickCloseBtn);
+    this.$closeBtn = $('.as-select-list-box-close-btn', this);
+    if (this.$closeBtn)//mobile ref
+        this.$closeBtn.on('click', this.eventHandler.clickCloseBtn);
 
 };
 
