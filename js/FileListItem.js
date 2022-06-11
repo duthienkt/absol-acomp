034bd4f3f94d8d6c81131516ab36c8213842983c
duthienkt <blueskythien2010@live.com>
2022-06-11 15:55:37 +0700

    - quickmenu

--- js/FileListItem.js
+++ js/FileListItem.js
@@ -1,8 +1,10 @@
-import ACore, {$, _} from "../ACore";
+import ACore, { $, _ } from "../ACore";
 import '../css/filelistinput.css';
 import ExtIcons from "../assets/exticons/catalog.json";
 import MessageInput from "./MessageInput";
 import { fileInfoOf, isURLAddress } from "./utils";
+import QuickMenu from "./QuickMenu";
+import LanguageSystem from "absol/src/HTML5/LanguageSystem";
 
 /***
  * @extends AElement
@@ -11,9 +13,65 @@ import { fileInfoOf, isURLAddress } from "./utils";
 function FileListItem() {
     this.$icon = $('.as-file-list-item-icon', this);
     this.$name = $('.as-file-list-item-name', this);
+    this.$quickMenuBtn = $('.as-file-list-item-quick-btn', this);
     this.value = null;
     this.fileType = null;
     this.fileName = '';
+    QuickMenu.toggleWhenClick(this.$quickMenuBtn, {
+        getMenuProps: () => {
+            var list = this.parentElement;
+            if (list.disabled) return;
+            var self = this;
+            var itemElt = this
+            var menuItems = [];
+            menuItems.push({
+                text: LanguageSystem.getText('txt_download') || "Download",
+                icon: 'span.mdi.mdi-download',
+                cmd: 'download'
+            });
+            if (!list.readOnly) {
+                if (itemElt)
+                    menuItems.push({
+                        text: LanguageSystem.getText('txt_delete') || "Delete",
+                        icon: 'span.mdi.mdi-delete',
+                        cmd: 'delete'
+                    });
+                menuItems.push(
+                    {
+                        text: LanguageSystem.getText('txt_delete_all') || "Delete All",
+                        icon: 'span.mdi.mdi-delete-empty',
+                        cmd: 'delete_all'
+                    });
+            }
+            return {
+                items: menuItems
+            };
+        },
+        onSelect: function (menuItem) {
+            var itemElt = this;
+            var self = this.parentElement;
+            var files;
+            switch (menuItem.cmd) {
+                case 'download':
+                    self.downloadFileItemElt(itemElt);
+                    break;
+                case 'delete':
+                    self.deleteFileItemElt(itemElt);
+                    self.emit('change', {
+                        type: 'change',
+                        item: itemElt.fileData,
+                        target: this,
+                        action: 'delete'
+                    }, this);
+                    break;
+                case 'delete_all':
+                    files = self.files;
+                    self.files = [];
+                    self.emit('change', { type: 'change', items: files, target: this, action: 'delete_all' }, this);
+                    break;
+            }
+        }.bind(this)
+    });
 }
 
 FileListItem.tag = 'FileListItem'.toLowerCase();
@@ -32,7 +90,12 @@ FileListItem.render = function () {
             },
             {
                 class: 'as-file-list-item-name',
-                child: {text: '..'}
+                child: { text: '..' }
+            },
+            {
+                tag: 'button',
+                class: 'as-file-list-item-quick-btn',
+                child: 'span.mdi.mdi-dots-vertical'
             }
         ]
     });
@@ -48,7 +111,8 @@ FileListItem.property.fileType = {
                 this.$icon.src = MessageInput.iconAssetRoot + '/' + value + '.svg';
             else
                 this.$icon.src = MessageInput.iconAssetRoot + '/' + 'blank' + '.svg';
-        } else {
+        }
+        else {
             this.$icon.src = MessageInput.iconAssetRoot + '/' + 'blank' + '.svg';
         }
     },
@@ -71,7 +135,7 @@ FileListItem.property.value = {
     set: function (value) {
         value = value || null;
         var info = fileInfoOf(value);
-        var type = info.type ||null;
+        var type = info.type || null;
         var size = info.size || null;
         var name = info.name || null;
         this._value = value;
@@ -80,7 +144,8 @@ FileListItem.property.value = {
         this.fileType = type;
         if (value) {
             this.addClass('as-has-value');
-        } else {
+        }
+        else {
             this.removeClass('as-has-value');
         }
     },
