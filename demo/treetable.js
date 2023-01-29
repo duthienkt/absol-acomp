7a0dfbe8f2c709d46272140878376c2e7ede68bc
duthienkt <blueskythien2010@live.com>
2023-01-29 16:36:46 +0700

    - demo

--- demo/treetable.js
+++ demo/treetable.js
@@ -1,15 +1,20 @@
 var tree = require('./demo_tree_list.js');
-console.log(tree)
 var count = 0;
 
-var searchInput = absol._({
+function render(o) {
+    return absol._(o).addTo(document.body);
+}
+
+render('<h2>apdater.type = "default" | Ø</h2>')
+
+var searchInput = render({
     tag: 'searchtextinput',
     style: {
         margin: '5px'
     }
-}).addTo(document.body);
+});
 
-var table = absol._({
+var table = render({
     tag: 'treetable',
     props: {
         searchInput: searchInput,
@@ -72,11 +77,63 @@ var table = absol._({
     }
 });
 
-absol._({
+render({
     tag: 'tablevscroller',
 
     style: {
         height: '50vh',
     },
     child: table
-}).addTo(document.body);
+});
+
+render('<h2>adapter.type = "struct"</h2>');
+var adapterData = {
+    type: 'struct',
+    propertyNames: ['text', 'value', 'starttime', 'endtime'],
+    treeBy: 'text',
+    propertyDescriptors: {
+        text: {
+            text: 'Tên',
+            type: 'text'
+        },
+        value: {
+            text: 'Định danh',
+            type: 'number'
+        },
+        starttime: {
+            text: 'Bắt đầu',
+            type: 'Date'
+        },
+        endtime: {
+            text: 'Kết thúc',
+            type: 'Date'
+        }
+    },
+    records: tree.map(function visit(it){
+        var res = {
+            text: it.text,
+            value: it.value
+        };
+        var desc = (it.desc || '').split(/\s*-\s*/);
+        if (desc.length > 0) {
+            res.starttime = absol.datetime.parseDateTime(desc[0], 'dd/MM/yyyy');
+            if (desc.length > 1) {
+                res.endtime = absol.datetime.parseDateTime(desc[1], 'dd/MM/yyyy');
+            }
+        }
+        if (it.items && it.items.length > 0) {
+            res.__children__ = it.items.map(visit);
+        }
+        return res;
+    })
+};
+
+render({
+    tag: 'treetable',
+    props: {
+        adapter: adapterData
+    }
+});
+
+console.log(tree)
+
