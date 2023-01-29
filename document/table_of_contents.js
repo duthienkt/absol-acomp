d4affc83e2b5c2231d7b1a6aa17c91a5184b39a5
duthienkt <blueskythien2010@live.com>
2023-01-29 22:00:42 +0700

    - doc

--- document/table_of_contents.js
+++ document/table_of_contents.js
@@ -1,21 +1,21 @@
+var rawFolder = 'https://raw.githubusercontent.com/duthienkt/absol-acomp/master';
 var toc = {
     name: 'absol-acomp',
     type: 'package',
-    children:[
+    children: [
         {
             name: 'Dom Component',
             type: 'group',
-            children: Object.keys(absol.AComp.core.creator).map(key=>{
+            children: Object.keys(absol.AComp.core.creator).map(key => {
                 var clazz = absol.AComp.core.creator[key];
                 var name = (clazz + '').match(/function\s+([a-zA-Z0-9_]+)/);
-                name = (name && name[1]) ||'';
-
+                name = (name && name[1]) || key;
                 var res = {
                     type: 'dom-class',
                     tagName: key,
-                    name: name
+                    name: name,
+                    href: [rawFolder, 'document', 'components', name].join('/')
                 };
-
 
                 return res;
 
