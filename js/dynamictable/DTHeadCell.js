88258ea7b3757d5537129dd88255e09318327c10
duthienkt <blueskythien2010@live.com>
2024-01-10 00:04:51 +0700

    - multi sort-key in colum

--- js/dynamictable/DTHeadCell.js
+++ js/dynamictable/DTHeadCell.js
@@ -1,4 +1,20 @@
 import { $$, _, $ } from "../../ACore";
+import Follower from "../Follower";
+import { findMaxZIndex } from "../utils";
+
+
+var implicitSortKeyArr = key => {
+    var res;
+    if (typeof key === 'string') {
+        res = key.split(/[\s,;]+/);
+    }
+    else if (key instanceof Array) {
+        res = key.slice();
+    }
+    else res = [];
+    res = res.filter(k => !!k).map(k => k + '');
+    return res;
+}
 
 /***
  *
@@ -34,17 +50,99 @@ DTHeadCell.prototype.nextSortState = function (event) {
     if (!this.elt.attr('data-sort-key')) return;
     var c = this.elt.attr('data-sort-order');
     var n = { none: 'ascending', ascending: 'descending', descending: 'none' }[c] || 'none';
+    var sortKeyArr = implicitSortKeyArr(this.data.sortKey);
 
-    //todo
-    console.log("TODO this??");
-    $$('th', this.row.elt).forEach(elt => {
-        if (elt === this.elt) return;
-        if (elt.attr('data-sort-key')) {
-            elt.attr('data-sort-order', 'none');
+    var sortBtn = this.$sortBtn;
+    var followerElt, menuElt, items;
+
+    if (sortKeyArr.length === 1) {
+        $$('th', this.row.elt).forEach(elt => {
+            if (elt === this.elt) return;
+            if (elt.attr('data-sort-key')) {
+                elt.attr('data-sort-order', 'none');
+            }
+        })
+        this.elt.attr('data-sort-order', n);
+        this.row.head.table.wrapper.requestQuery();
+    }
+    else {
+        items = [{
+            text: 'Không sort',
+            value: { key: sortKeyArr.join(';'), order: 'none' },
+            icon: 'span.mdi.mdi-sort-variant-off'
+        }];
+        if (c === 'none') {
+            items[0].extendStyle = { color: '#007bff' };
         }
-    })
-    this.elt.attr('data-sort-order', n);
-    this.row.head.table.wrapper.requestQuery();
+        sortKeyArr.forEach(key => {
+            items.push('-------');
+            var text = this.data.sortMenu && this.data.sortMenu[key] && this.data.sortMenu[key][0];
+            text = text || ("Tăng theo " + key);
+            var item = {
+                icon: 'span.mdi.mdi-sort-ascending',
+                text: text,
+                value: { key: key, order: 'ascending' }
+            };
+            if (key === this.elt.attr('data-sort-key') && c === 'ascending') {
+                item.extendStyle = { color: '#007bff' }
+            }
+            items.push(item);
+            text = this.data.sortMenu && this.data.sortMenu[key] && this.data.sortMenu[key][1];
+            text = text || ("Tăng theo " + key);
+            item = {
+                icon: 'span.mdi.mdi-sort-descending',
+                text: text,
+                value: { key: key, order: 'descending' }
+            };
+            if (key === this.elt.attr('data-sort-key') && c === 'descending') {
+                item.extendStyle = { color: '#007bff' }
+            }
+            items.push(item);
+        });
+        menuElt = _({
+            tag: 'vmenu',
+            props: {
+                items: items
+            }, on: {
+                press: event => {
+                    var value = event.menuItem.value;
+                    $$('th', this.row.elt).forEach(elt => {
+                        if (elt === this.elt) return;
+                        if (elt.attr('data-sort-key')) {
+                            elt.attr('data-sort-order', 'none');
+                        }
+                    })
+                    this.elt.attr('data-sort-order', value.order);
+                    this.elt.attr('data-sort-key', value.key);
+                    this.row.head.table.wrapper.requestQuery();
+                    finish();
+                }
+            }
+        });
+        followerElt = _({
+            tag: Follower,
+            class: 'absol-context-menu-anchor',
+            style: {
+                zIndex: findMaxZIndex(sortBtn) + 22
+            },
+            child: [
+                menuElt
+            ],
+            props: {
+                followTarget: sortBtn
+            }
+        }).addTo(document.body);
+        var finish = () => {
+            document.removeEventListener('click', finish);
+            followerElt.remove();
+        }
+        setTimeout(() => {
+            followerElt.addStyle('visibility', 'visible').addStyle('opacity', 1);
+
+        }, 5)
+
+    }
+
 };
 
 DTHeadCell.prototype.updateCopyEltSize = function () {
@@ -92,8 +190,9 @@ Object.defineProperty(DTHeadCell.prototype, 'elt', {
             this._elt.attr('data-col-id', this.data.id + '');
         }
         this.row.head.table.adapter.renderHeadCell(this._elt, this.data, this);
-        if (this.data.sortKey) {
-            this._elt.attr('data-sort-key', this.data.sortKey);
+        var sortKeyArr = implicitSortKeyArr(this.data.sortKey)
+        if (sortKeyArr.length > 0) {
+            this._elt.attr('data-sort-key', sortKeyArr.join(';'));
             this._elt.attr('data-sort-order', 'none');
         }
         this.$sortBtn = _({
