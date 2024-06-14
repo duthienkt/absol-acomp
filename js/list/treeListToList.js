59b12231a6abd6f2d1a89448020ab4429373aadd
duthienkt <blueskythien2010@live.com>
2024-06-14 19:14:20 +0700

    * fix type checking

--- js/list/treeListToList.js
+++ js/list/treeListToList.js
@@ -7,13 +7,15 @@ export default function treeListToList(items) {
     var arr = [];
     function visit(level, node) {
         Object.defineProperty(node, 'level', {
-           configurable: true,
+            configurable: true,
             writable: true,
             value: level
         });
         arr.push(node);
-        if (node.items && node.items.length > 0) node.items.forEach(visit.bind(null, level + 1));
+        if (Array.isArray(node.items)  && node.items.length > 0) node.items.forEach(visit.bind(null, level + 1));
     }
-    items.forEach(visit.bind(null, 0));
+
+    if (items && items.forEach)
+        items.forEach(visit.bind(null, 0));
     return arr;
 }
