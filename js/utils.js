48406375f2a0f07bec63d688135f538f955cee19
duthienkt <blueskythien2010@live.com>
2024-07-01 23:47:33 +0700

    - export function

--- js/utils.js
+++ js/utils.js
@@ -1371,65 +1371,7 @@ export function replaceFileInObject(o, replacer) {
     });
 }
 
-export function revokeResource(o) {
-    if (!o) return;
-    var oc, ocs;
-    var keys, key;
-    if (Array.isArray(o)) {
-        while (o.length) {
-            oc = o.pop();
-            try {
-                revokeResource(oc);
-            } catch (err) {
-            }
-        }
-    }
-    else if (o.revokeResource) {
-        o.revokeResource();
-    }
-    else if (o.nodeType === 1 || o.nodeType === 3) {
-        if (o.revokeResource)
-            o.revokeResource();
-        else {
-            while (o.firstChild) {
-                oc =o.firstChild;
-                revokeResource(oc);
-                if (oc.selfRemove) {
-                    oc.selfRemove();
-                }
-                else {
-                    oc.remove();
-                }
-            }
-        }
-    }
-    else if (typeof o === "object") {
-        keys = [];
-        ocs = [];
-        for (key in o) {
-            keys.push(key);
-        }
-        while (keys.length) {
-            key = keys.pop();
-            ocs.push(o[keys]);
-            try {
-                delete o[key];
-            } catch (err) {
-            }
-        }
-        while (ocs.length) {
-            try {
-                revokeResource(ocs.pop());
-            } catch (err) {
-            }
-        }
-    }
-    ocs = undefined;
-    oc = undefined;
-    keys = undefined;
-    key = undefined;
-    o = undefined;
-}
+export {revokeResource} from 'absol/src/DataStructure/Object';
 
 export function isNone(x) {
     return x === null || x === undefined;
