ceece0f5bbb6293e3bc52c1bc73ae572919fd884
duthienkt <blueskythien2010@live.com>
2025-08-26 20:56:42 +0700

    - script for mac

--- sync_dist.sh
+++ sync_dist.sh
@@ -1,2 +1,5 @@
 cp -f -R ./js/ ../absol-full/node_modules/absol-acomp/js/
 cp -f -R ./css/ ../absol-full/node_modules/absol-acomp/css/
+
+cp -f -R ./js/ ../absol-form/node_modules/absol-acomp/js/
+cp -f -R ./css/ ../absol-form/node_modules/absol-acomp/css/
