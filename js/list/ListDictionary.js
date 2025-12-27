4ae8c1576da388110e49b608f089bfe9eff98935
duthienkt <blueskythien2010@live.com>
2025-12-27 23:52:02 +0700

    - more options

--- js/list/ListDictionary.js
+++ js/list/ListDictionary.js
@@ -3,10 +3,31 @@
  * @param {SelectionItem[]} arr
  * @constructor
  */
-import { keyStringOf } from "../utils";
+import { keyStringOf, legacyKeyStringOf } from "../utils";
 
-function ItemDictionary(arr, opt) {
-    this.opt = Object.assign({ depth: true }, opt || {});
+
+/**
+ * ListDictionary provides fast lookup and duplicate detection for a list of SelectionItem objects.
+ *
+ * @class
+ * @param {SelectionItem[]} arr - The array of selection items to index.
+ * @param {Object} [opt] - Optional settings.
+ * @param {boolean} [opt.depth=true] - Whether to scan nested items recursively.
+ * @param {boolean} [opt.legacy=false] - Use legacy key string function.
+ *
+ * @property {SelectionItem[]} arr - The original array of items.
+ * @property {Object} dict - Internal dictionary for fast lookup.
+ * @property {Array<string>} dupKeys - List of keys with duplicate items.
+ * @property {Function} keyStringOf - Function to generate key strings for items.
+ */
+function ListDictionary(arr, opt) {
+    this.opt = Object.assign({ depth: true, legacy: false }, opt || {});
+    if (this.opt.legacy) {
+        this.keyStringOf = legacyKeyStringOf;
+    }
+    else {
+        this.keyStringOf =  keyStringOf;
+    }
     this.arr = arr;
     this.dict = {};
     this.dupKeys = [];
@@ -14,37 +35,41 @@ function ItemDictionary(arr, opt) {
 }
 
 
-ItemDictionary.prototype.update = function () {
+ListDictionary.prototype.update = function () {
     this.dict = {};
     var dict = this.dict;
     var depth = this.opt.depth;
 
     var dupKeyDict = {};
 
-    function scan(arr) {
+    function scan(arr, parent) {
         var item;
         var key;
+        var holder;
         for (var i = 0; i < arr.length; ++i) {
             item = arr[i];
-            key = keyStringOf(item.value);
-            if (dict[key]) {
-                dict[key].dupItems = dict[key].dupItems || [];
-                dict[key].dupItems.push(item);
-                dupKeyDict[key] = 1;
+            key = this.keyStringOf(item.value);
+            holder = dict[key];
+            if (holder) {
+                holder.dupItems = dict[key].dupItems || [];
+                holder.dupItems.push(item);
+                dupKeyDict[key] =(dupKeyDict[key] || 0) + 1;
             }
             else {
-                dict[key] = {
+                holder = {
+                    pathIdx:parent? parent.pathIdx.concat([i]):[i],
                     idx: i,
                     item: item
                 };
+                dict[key] = holder;
             }
             if (depth && item.items && item.items.length > 0) {
-                scan(item.items);
+                scan(item.items, holder);
             }
         }
     }
 
-    scan(this.arr);
+    scan(this.arr, null);
     this.dupKeys = Object.keys(dupKeyDict);
 };
 
@@ -53,8 +78,8 @@ ItemDictionary.prototype.update = function () {
  * @param {String | Number} value
  * @returns {SelectionItem | null}
  */
-ItemDictionary.prototype.getItemByValue = function (value) {
-    var iDict = this.dict[keyStringOf(value)];
+ListDictionary.prototype.getItemByValue = function (value) {
+    var iDict = this.dict[this.keyStringOf(value)];
     if (iDict) return iDict.item;
     return null;
 };
@@ -64,15 +89,19 @@ ItemDictionary.prototype.getItemByValue = function (value) {
  * @param {String | Number} value
  * @returns {Array<SelectionItem> }
  */
-ItemDictionary.prototype.getAllItemByValue = function (value) {
-    var key = keyStringOf(value);
+ListDictionary.prototype.getAllItemByValue = function (value) {
+    var key = this.keyStringOf(value);
     var iDict = this.dict[key];
     if (iDict) return [iDict.item].concat(iDict.dupItems[key] || []);
     return [];
 };
 
-ItemDictionary.prototype.getDuplicateKeys = function () {
+/**
+ *
+ * @return {Array<string>}
+ */
+ListDictionary.prototype.getDuplicateKeys = function () {
     return this.dupKeys;
 };
 
-export default ItemDictionary;
+export default ListDictionary;
