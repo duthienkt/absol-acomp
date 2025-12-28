a6ec3a805577e1fe5e1193862c0a07ed7c741021
duthienkt <blueskythien2010@live.com>
2025-12-28 17:08:43 +0700

    - copy file script

---
+++ copy_dist.sh
@@ -0,0 +1,11 @@
+cp -R -f ./js/ ../absol-full/node_modules/absol-acomp/js/
+cp -R -f ./css/ ../absol-full/node_modules/absol-acomp/css/
+cp -f ./dev.js ../absol-full/node_modules/absol-acomp/
+
+cp -R -f ./js/ ../absol-form/node_modules/absol-acomp/js/
+cp -R -f ./css/ ../absol-form/node_modules/absol-acomp/css/
+cp -f ./dev.js ../absol-form/node_modules/absol-acomp/
+
+cp -R -f ./js/ ../absol-sheet/node_modules/absol-acomp/js/
+cp -R -f ./css/ ../absol-sheet/node_modules/absol-acomp/css/
+cp -f ./dev.js ../absol-sheet/node_modules/absol-acomp/
