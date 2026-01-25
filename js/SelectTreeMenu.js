d94503323f950704175c834f98250795a8658c57
duthienkt <blueskythien2010@live.com>
2026-01-25 12:40:01 +0700

    - mobile version

--- js/SelectTreeMenu.js
+++ js/SelectTreeMenu.js
@@ -1,9 +1,11 @@
 import '../css/selecttreemenu.css';
 import ACore from "../ACore";
 import OOP, { mixClass } from "absol/src/HTML5/OOP";
-import SelectMenu from "./SelectMenu2";
+import SelectMenu, { SMDropdownController, SMLifecycleController, SMMobileDropdownController } from "./SelectMenu2";
 import SelectTreeBox from "./SelectTreeBox";
 import { AbstractInput } from "./Abstraction";
+import BrowserDetector from "absol/src/Detector/BrowserDetector";
+import { MTreeModal } from "./selecttreemenu/MSelectTreeMenu";
 
 var _ = ACore._;
 var $ = ACore.$;
@@ -15,58 +17,53 @@ var $ = ACore.$;
  * @constructor
  */
 function SelectTreeMenu() {
+    this.isMobile = BrowserDetector.isMobile;
     this._items = [];
     this._value = null;
     this._lastValue = null;
     this.$holderItem = $('.absol-selectmenu-holder-item', this);
     this.$viewItem = $('.absol-selectmenu-holder-item selectlistitem', this);
-    /***
-     *
-     * @type {SelectTreeBox}
-     */
-    this.$selectlistBox = _({
-        tag: 'selecttreebox',
-        props: {
-            anchor: [1, 6, 2, 5]
-        },
-        on: {
-            preupdateposition: this.eventHandler.preUpdateListPosition
-        }
-    });
-    if (!this.$selectlistBox.cancelWaiting) {
-        this.$selectlistBox.cancelWaiting();
+    if (this.isMobile) {
+        /***
+         * @type {MTreeModal}
+         */
+        this.$selectlistBox = _({ tag: MTreeModal });
     }
+    else {
+        /***
+         *
+         * @type {SelectTreeBox}
+         */
+        this.$selectlistBox = _({
+            tag: 'selecttreebox',
+            props: {
+                anchor: [1, 6, 2, 5]
+            },
+            on: {
+            }
+        });
+    }
+
+    this.dropdownCtrl = this.isMobile? new SMMobileDropdownController(this): new SMDropdownController(this);
+    this.lifecycleCtrl = new SMLifecycleController(this);
+
     this.$selectlistBox.on('pressitem', this.eventHandler.selectListBoxPressItem);
-    this.$selectlistBox.followTarget = this;
-    this.$selectlistBox.sponsorElement = this;
-    this.$selectlistBox.unfollow();
+
     OOP.drillProperty(this, this.$selectlistBox, 'enableSearch');
 
     this._lastValue = "NOTHING_VALUE";
-    this._isFocus = false;
-    this.isFocus = false;
 
-    this.on('mousedown', this.eventHandler.click, true);
     AbstractInput.call(this);
 }
 
-mixClass(SelectTreeMenu, AbstractInput);
+mixClass(SelectTreeMenu, AbstractInput, SelectMenu);
 
 SelectTreeMenu.tag = 'SelectTreeMenu'.toLowerCase();
 
-SelectTreeMenu.prototype.styleHandlers = Object.assign({},SelectMenu.property.styleHandlers);
-SelectTreeMenu.prototype.extendStyle = Object.assign({}, SelectMenu.property.extendStyle);
 
 SelectTreeMenu.render = function () {
     return SelectMenu.render().addClass('as-select-tree-menu');
 };
-
-SelectTreeMenu.eventHandler = Object.assign({}, SelectMenu.eventHandler);
-SelectTreeMenu.property = Object.assign(SelectTreeMenu.property || {}, SelectMenu.property);
-
-
-Object.assign(SelectTreeMenu.prototype, SelectMenu.prototype);
-// console.log(SelectTreeMenu.property.isFocus);
 
 
 ACore.install(SelectTreeMenu);
