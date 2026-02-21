a0975592134ae2dc7935b43b8e509e02dbef5239
duthienkt <blueskythien2010@live.com>
2026-02-21 21:36:40 +0700

    - highlight text property

--- js/CheckTreeItem.js
+++ js/CheckTreeItem.js
@@ -3,6 +3,7 @@ import { ExpNode } from "./ExpTree";
 import { getDescriptionOfListItem, getTextOfListItem } from "./SelectListItem";
 import '../css/checktreeitem.css';
 import { hitElement } from "absol/src/HTML5/EventEmitter";
+import { nonAccentVietnamese } from "absol/src/String/stringFormat";
 
 
 /***
@@ -10,15 +11,47 @@ import { hitElement } from "absol/src/HTML5/EventEmitter";
  * @constructor
  */
 function CheckTreeItem() {
+    /**
+     * @type {any}
+     * @private
+     */
     this._itemData = null;
+    /**
+     * @type {HTMLSpanElement|AElement}
+     * Tên của node (hiển thị tên mục)
+     */
     this.$name = $('span.absol-exp-node-name', this);
+    /**
+     * @type {HTMLSpanElement|AElement}
+     * Mô tả của node (hiển thị mô tả mục)
+     */
     this.$desc = $('span.absol-exp-node-desc', this);
     this.$iconCtn = $('div.absol-exp-node-ext-icon', this);
+    /**
+     * @type {HTMLElement|AElement}
+     * Hiển thị mức (level) của node trong cây
+     */
     this.$level = $('.absol-exp-node-level', this);
     this.$checkbox = $('checkboxbutton', this)
         .on('change', this.eventHandler.checkboxChange);
     this.$toggleIcon = $('toggler-ico', this);
     this.on('click', this.eventHandler.click);
+    /**
+     * @type {string}
+     * @name name
+     * @memberOf CheckTreeItem#
+     */
+
+    /**
+     * @type {string}
+     * @name text
+     * @memberOf CheckTreeItem#
+     */
+    /**
+     * @type {string}
+     * @name highlightedText
+     * @memberOf CheckTreeItem#
+     */
 }
 
 CheckTreeItem.tag = 'CheckTreeItem'.toLowerCase();
@@ -43,10 +76,72 @@ CheckTreeItem.render = function () {
 CheckTreeItem.property = {};
 
 CheckTreeItem.property.icon = ExpNode.property.icon;
-CheckTreeItem.property.text = ExpNode.property.name;
 CheckTreeItem.property.level = ExpNode.property.level;
 CheckTreeItem.property.desc = ExpNode.property.desc;
 CheckTreeItem.property.status = ExpNode.property.status;
+
+CheckTreeItem.prototype.updateName = function () {
+    this.$name.clearChild();
+    var value = this.name;
+    var highlightedText = this.highlightedText.toLowerCase();
+    var parts = [];
+    var idx;
+    if (value && value.length > 0) {
+        if (highlightedText) {
+            while (value) {
+                idx = nonAccentVietnamese(value.toLowerCase()).indexOf(highlightedText);
+                if (idx < 0) {
+                    parts.push(_({ text: value }));
+                    value = '';
+                }
+                else {
+                    if (idx > 0) {
+                        parts.push(_({ text: value.substring(0, idx) }));
+                    }
+                    parts.push(_({ tag: 'mark', child: { text: value.substring(idx, idx + highlightedText.length) } }));
+                    value = value.substring(idx + highlightedText.length);
+                }
+            }
+            this.$name.addChild(parts);
+        }
+        else {
+            this.$name.addChild(_({ text: value }));
+        }
+    }
+
+};
+
+CheckTreeItem.property.name = {
+    set: function (value) {
+        value = value + '';
+        this._name = value;
+        this.updateName();
+    },
+    get: function () {
+        return this._name || '';
+    }
+};
+
+CheckTreeItem.property.text = {
+    set: function (value) {
+        this.name = value;
+    },
+    get: function () {
+        return this.name;
+    }
+};
+
+
+CheckTreeItem.property.highlightedText = {
+    set: function (value) {
+        value = value || '';
+        this._highlightedText = (value + '').trim();
+        this.updateName();
+    },
+    get: function () {
+        return this._highlightedText || '';
+    }
+};
 
 
 CheckTreeItem.property.data = {
@@ -92,8 +187,8 @@ CheckTreeItem.property.selected = {
         }
     },
     get: function () {
-        if ( this.$checkbox.disabled) return 'empty';
-        if (this.$checkbox.checked) return  'all';
+        if (this.$checkbox.disabled) return 'empty';
+        if (this.$checkbox.checked) return 'all';
         else if (this.$checkbox.minus) return 'child';
         return 'none';
     }
@@ -116,23 +211,23 @@ CheckTreeItem.property.noSelect = {
 CheckTreeItem.property.readOnly = {
     /**
      * @this CheckTreeItem
-      * @param value
+     * @param value
      */
-  set: function (value) {
-      if (value) {
-          this.addClass('as-read-only');
-          this.$checkbox.readOnly = true;
-          this.$checkbox.addClass('as-border-none');
-      }
-      else {
-          this.removeClass('as-read-only');
-          this.$checkbox.readOnly = false;
-          this.$checkbox.removeClass('as-border-none');
-      }
-  },
-  get: function () {
-      return this.hasClass('as-read-only');
-  }
+    set: function (value) {
+        if (value) {
+            this.addClass('as-read-only');
+            this.$checkbox.readOnly = true;
+            this.$checkbox.addClass('as-border-none');
+        }
+        else {
+            this.removeClass('as-read-only');
+            this.$checkbox.readOnly = false;
+            this.$checkbox.removeClass('as-border-none');
+        }
+    },
+    get: function () {
+        return this.hasClass('as-read-only');
+    }
 };
 
 
